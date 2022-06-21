400c35bfad73bbf173cf2be26eeb9b4dbd98f730
unknown <arthur.lolico@gmail.com>
2022-06-21 11:57:54 -0300

    docs: basic page with redirect

--- docs/website/src/pages/index.js
+++ docs/website/src/pages/index.js
@@ -7,5 +7,31 @@ import { Redirect } from '@docusaurus/router';
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
 
-  return <Redirect to="docs/carlin/intro" />;
+  // return <Redirect to="docs/carlin/intro" />;
+
+  return (
+    <Layout
+    title={`Hello from ${siteConfig.title}`}
+    description="Description will go into a meta tag in <head />"
+  >
+    <main>
+      <div className="container text--center">
+        <div className="row">
+          <div className ="col">
+            <img src="/img/undraw_typewriter.svg"></img>
+            <h3>Engineering Guide</h3>
+          </div>
+          <div className ="col">
+            <img src="/img/undraw_react.svg"></img>
+            <h3 className="featureHeading_TLGJ">Modules</h3>
+          </div>
+          <div className ="col">
+            <img src="/img/undraw_around_the_world.svg"></img>
+            <h3 class="featureHeading_TLGJ">Carlin</h3>
+          </div>
+                </div>
+        </div>
+    </main>
+  </Layout>
+  );
 }
