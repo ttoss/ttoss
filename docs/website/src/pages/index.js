5098205cc1859977cf6554e0698781bc37166f50
Pedro Arantes <arantespp@gmail.com>
2022-06-28 18:08:12 -0300

    fix: add config deps (#11)

--- docs/website/src/pages/index.js
+++ docs/website/src/pages/index.js
@@ -1,6 +1,6 @@
+import Layout from '@theme/Layout';
 import React from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
@@ -9,27 +9,27 @@ export default function Home() {
 
   return (
     <Layout
-    title={`Hello from ${siteConfig.title}`}
-    description="Description will go into a meta tag in <head />"
-  >
-    <main>
-      <div className="container text--center">
-        <div className="row">
-          <div className ="col">
-            <img src="/img/undraw_typewriter.svg"></img>
-            <h3>Engineering Guide</h3>
-          </div>
-          <div className ="col">
-            <img src="/img/undraw_react.svg"></img>
-            <h3 className="featureHeading_TLGJ">Modules</h3>
-          </div>
-          <div className ="col">
-            <img src="/img/undraw_around_the_world.svg"></img>
-            <h3 class="featureHeading_TLGJ">Carlin</h3>
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />"
+    >
+      <main>
+        <div className="container text--center">
+          <div className="row">
+            <div className="col">
+              <img src="/img/undraw_typewriter.svg" alt="image1"></img>
+              <h3>Engineering Guide</h3>
+            </div>
+            <div className="col">
+              <img src="/img/undraw_react.svg" alt="image2"></img>
+              <h3 className="featureHeading_TLGJ">Modules</h3>
+            </div>
+            <div className="col">
+              <img src="/img/undraw_around_the_world.svg" alt="image3"></img>
+              <h3 className="featureHeading_TLGJ">Carlin</h3>
+            </div>
           </div>
         </div>
-      </div>
-    </main>
-  </Layout>
+      </main>
+    </Layout>
   );
 }
