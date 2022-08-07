35561d61c9ee1012ed7ecabbfa0519677fc90374
Pedro Arantes <arantespp@gmail.com>
2022-08-07 17:07:05 -0300

    fix: css parse jest

--- packages/components/src/index.spec.tsx
+++ packages/components/src/index.spec.tsx
@@ -4,6 +4,8 @@ test.each([
   componentsModule.Accordion,
   componentsModule.InstallPwa,
   componentsModule.Modal,
+  componentsModule.ToastContainer,
+  componentsModule.toast,
 ])('should export components %#', (Component) => {
   expect(Component).toBeDefined();
 });
