8db7708afb7b853d7f1a1f74c5a5df9051af0251
Pedro Arantes <arantespp@gmail.com>
2022-10-05 11:43:56 -0300

    fix: add quiet to eslint

--- packages/config/src/lintstaged.ts
+++ packages/config/src/lintstaged.ts
@@ -1,7 +1,7 @@
 import { configCreator } from './configCreator';
 
 export const defaultConfig: any = {
-  '*.{js,jsx,ts,tsx}': 'eslint --fix',
+  '*.{js,jsx,ts,tsx}': 'eslint --quiet --fix',
   '*.{md,mdx,html,json,yml,yaml}': 'prettier --write',
   'package.json': 'prettier-package-json --write',
 };
