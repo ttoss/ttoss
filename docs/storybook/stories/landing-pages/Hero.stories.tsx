79a9f937e3298321484a1d56aff7a2d84c25ca1d
Rayza <rayza.ocr@gmail.com>
2022-10-17 11:44:15 -0300

    feat: creating hero stories

--- docs/storybook/stories/landing-pages/Hero.stories.tsx
+++ docs/storybook/stories/landing-pages/Hero.stories.tsx
@@ -1,4 +1,4 @@
-import { Hero } from '@ttoss/landing-pages';
+import { Hero, HeroProps } from '@ttoss/landing-pages';
 import { Meta, Story } from '@storybook/react';
 
 export default {
@@ -6,8 +6,32 @@ export default {
   component: Hero,
 } as Meta;
 
-const Template: Story = () => {
-  return <Hero />;
+const Template: Story<HeroProps> = (args) => {
+  return <Hero {...args} />;
 };
 
-export const Example = Template.bind({});
+const exampleObj = {
+  headerText: 'h1: Header text',
+  subHeaderText: 'h2: Subheader text',
+  alignment: 'center',
+  imageSrc:
+    'https://tt-sandbox-general-purpose.s3.amazonaws.com/modules/close-up-prison-bars-1.png',
+  imageAlt: 'Hero section',
+  ctaLabel: 'Clique aqui',
+};
+
+export const ExampleCenter = Template.bind({});
+
+ExampleCenter.args = {
+  ...exampleObj,
+  alignment: 'center',
+} as HeroProps;
+
+export const ExampleLeft = Template.bind({});
+
+ExampleLeft.args = {
+  ...exampleObj,
+  imageSrc:
+    'https://tt-sandbox-general-purpose.s3.amazonaws.com/modules/brutality-1.png',
+  alignment: 'left',
+} as HeroProps;
