f220d35cd5015f8c3b0422a89daeedc5f646d089
Pedro Arantes <arantespp@gmail.com>
2022-11-16 15:39:25 -0300

    feat: add stories Heading (#63)

---
+++ docs/storybook/stories/ui/Heading.stories.tsx
@@ -0,0 +1,29 @@
+import { Flex, Heading } from '@ttoss/ui';
+import { Meta, Story } from '@storybook/react';
+
+export default {
+  title: 'UI/Heading',
+  component: Heading,
+  argTypes: {
+    text: {
+      control: 'text',
+      defaultValue: 'Text for Heading ',
+    },
+  },
+} as Meta;
+
+const as = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] as const;
+
+const Template: Story = (args) => {
+  return (
+    <Flex sx={{ flexDirection: 'column', gap: 2 }}>
+      {as.map((tag) => (
+        <Heading as={tag} variant={tag} key={tag}>
+          {args.text} {tag}
+        </Heading>
+      ))}
+    </Flex>
+  );
+};
+
+export const Example = Template.bind({});
