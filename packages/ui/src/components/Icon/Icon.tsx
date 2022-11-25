5d30d22ecaa932692ed04decd9f8be06abd34e1b
Rayza <rayza.ocr@gmail.com>
2022-11-25 11:28:20 -0300

    feat: updating Icon props

--- packages/ui/src/components/Icon/Icon.tsx
+++ packages/ui/src/components/Icon/Icon.tsx
@@ -1,15 +1,15 @@
 import { IconProps as IconPropsUI, Icon as IconUI } from '@iconify/react';
 import { Text, TextProps } from '../Text/Text';
 
-export type IconProps = {
+export type IconProps = TextProps & {
   icon: IconPropsUI['icon'];
-  sx?: TextProps['sx'];
+  iconProps?: Omit<IconPropsUI, 'icon'>;
 };
 
-export const Icon = ({ icon, sx }: IconProps) => {
+export const Icon = ({ icon, sx, iconProps, ...props }: IconProps) => {
   return (
-    <Text sx={sx}>
-      <IconUI icon={icon} />
+    <Text sx={sx} {...props}>
+      <IconUI {...iconProps} icon={icon} />
     </Text>
   );
 };
