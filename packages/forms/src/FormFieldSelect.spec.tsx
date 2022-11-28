9d58b9a318dcbefdeb9079e3534b92c4925760cb
Rayza <rayza.ocr@gmail.com>
2022-11-28 20:27:53 -0300

    feat: created FormFieldSelect component

---
+++ packages/forms/src/FormFieldSelect.spec.tsx
@@ -0,0 +1,70 @@
+import * as yup from 'yup';
+import { Button } from '@ttoss/ui';
+import { Form } from './Form';
+import { FormFieldSelect } from './FormFieldSelect';
+import { render, screen, userEvent } from '@ttoss/test-utils';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+const RADIO_OPTIONS = [
+  { value: '', label: 'Select a car' },
+  { value: 'Ferrari', label: 'Ferrari' },
+  { value: 'Mercedes', label: 'Mercedes' },
+  { value: 'BMW', label: 'BMW' },
+];
+
+test('call onSubmit with correct data', async () => {
+  const user = userEvent.setup({ delay: null });
+
+  const onSubmit = jest.fn();
+
+  const RenderForm = () => {
+    const formMethods = useForm();
+
+    return (
+      <Form {...formMethods} onSubmit={onSubmit}>
+        <FormFieldSelect name="car" label="Cars" options={RADIO_OPTIONS} />
+        <Button type="submit">Submit</Button>
+      </Form>
+    );
+  };
+
+  render(<RenderForm />);
+
+  await user.selectOptions(screen.getByText('Select a car'), ['BMW']);
+
+  await user.click(screen.getByText('Submit'));
+
+  expect(onSubmit).toHaveBeenCalledWith({ car: 'BMW' });
+});
+
+test('should display error messages', async () => {
+  const user = userEvent.setup({ delay: null });
+
+  const onSubmit = jest.fn();
+
+  const schema = yup.object({
+    car: yup.string().required('Car is required'),
+  });
+
+  const RenderForm = () => {
+    const formMethods = useForm({
+      mode: 'all',
+      resolver: yupResolver(schema),
+    });
+
+    return (
+      <Form {...formMethods} onSubmit={onSubmit}>
+        <FormFieldSelect name="car" label="Cars" options={RADIO_OPTIONS} />
+
+        <Button type="submit">Submit</Button>
+      </Form>
+    );
+  };
+
+  render(<RenderForm />);
+
+  await user.click(screen.getByText('Submit'));
+
+  expect(await screen.findByText('Car is required')).toBeInTheDocument();
+});
