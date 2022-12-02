21b3bdece87bcb0824cf61ab2a7beb0012ab277d
RayzaOliveira <rayza.ocr@gmail.com>
2022-12-02 20:00:20 -0300

    feat: exported things from react-hook-form

--- packages/forms/src/index.ts
+++ packages/forms/src/index.ts
@@ -1,3 +1,4 @@
 export { Form } from './Form';
 export { FormField } from './FormField';
 export { yupResolver } from '@hookform/resolvers/yup';
+export { useController, FieldValues, Path, FieldName } from 'react-hook-form';
