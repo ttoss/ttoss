4e2c4c7477ce994ede1f1eca87f66ac93c5e98f2
Pedro Arantes <arantespp@gmail.com>
2022-12-07 15:55:06 -0300

    fix: gtmid and pixel inverted

--- packages/landing-pages/src/NextDocument/NextDocument.tsx
+++ packages/landing-pages/src/NextDocument/NextDocument.tsx
@@ -28,6 +28,17 @@ export class NextDocument extends Document {
           {this.gtmId && (
             <script
               dangerouslySetInnerHTML={{
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${this.gtmId}');`,
+              }}
+            />
+          )}
+          {this.pixelId && (
+            <script
+              dangerouslySetInnerHTML={{
                 __html: `  !function(f,b,e,v,n,t,s)
                   {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
                   n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -38,17 +49,6 @@ export class NextDocument extends Document {
                   'https://connect.facebook.net/en_US/fbevents.js');
                   fbq('init', ${this.pixelId});
                   fbq('track', 'PageView');`,
-              }}
-            />
-          )}
-          {this.pixelId && (
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${this.gtmId}');`,
               }}
             />
           )}
