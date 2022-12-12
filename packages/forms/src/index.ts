43a897353f98033c47745697be8519d44040cd8b
Pedro Arantes <arantespp@gmail.com>
2022-12-12 12:08:47 -0300

    fix: export all form fields

--- packages/forms/src/index.ts
+++ packages/forms/src/index.ts
@@ -1,5 +1,10 @@
-export { Form } from './Form';
-export { FormField } from './FormField';
 export { yupResolver } from '@hookform/resolvers/yup';
 export * from 'react-hook-form';
 export * as yup from 'yup';
+
+export { Form } from './Form';
+export { FormField } from './FormField';
+export { FormFieldCheckbox } from './FormFieldCheckbox';
+export { FormFieldInput } from './FormFieldInput';
+export { FormFieldRadio } from './FormFieldRadio';
+export { FormFieldSelect } from './FormFieldSelect';
