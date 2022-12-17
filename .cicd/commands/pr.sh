57c09c6ce6622dbb17a57f987a93416fe5f31970
Pedro Arantes <arantespp@gmail.com>
2022-12-17 12:01:11 -0300

    ci: update cicd commands

--- .cicd/commands/pr.sh
+++ .cicd/commands/pr.sh
@@ -1,13 +1,6 @@
 # Build config to run lint-staged for lint and version bump
 yarn turbo run build --filter=@ttoss/config...
 
-# Lint
-yarn turbo run lint
-
-# Build all packages since main and their dependencies
-# https://turbo.build/repo/docs/core-concepts/monorepos/filtering#include-dependencies-of-matched-workspaces
-yarn turbo run build --filter=[main]...
-
-# Test and deploy all packages since main and their dependent packages
+# Lint, test, build, and deploy all packages since main and their dependent packages.
 # https://turbo.build/repo/docs/core-concepts/monorepos/filtering#include-dependents-of-matched-workspaces
-yarn turbo test deploy --filter=...[main]
+yarn turbo run lint build test deploy --filter=...[main]
