6d441d09ac40e31532f232c3af816c7bc3a27371
Pedro Arantes <arantespp@gmail.com>
2022-12-20 11:34:01 -0300

    feat: add schema validation on appsync api (#115)

--- packages/appsync-api/src/cli.ts
+++ packages/appsync-api/src/cli.ts
@@ -11,6 +11,9 @@ if (argv._.includes('build-schema')) {
   const sdl =
     template.Resources[AppSyncGraphQLSchemaLogicalId].Properties.Definition;
 
+  // eslint-disable-next-line no-console
+  console.log(sdl);
+
   /**
    * Save to schema/schema.graphql. schema folder might not exist.
    */
