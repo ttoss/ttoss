176038d2e759338ac39bdf9f9b0bbc2d9d1ecb08
Pedro Arantes <arantespp@gmail.com>
2022-12-19 22:59:32 -0300

    docs: blog post

--- docs/website/docusaurus.config.js
+++ docs/website/docusaurus.config.js
@@ -159,10 +159,10 @@ const config = {
           {
             title: 'More',
             items: [
-              // {
-              //   label: 'Blog',
-              //   to: '/blog',
-              // },
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
