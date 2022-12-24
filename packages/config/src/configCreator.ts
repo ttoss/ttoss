f3bd7f2b310fe6d0c6478a6febb4d71077ae1ae5
Pedro Arantes <arantespp@gmail.com>
2022-12-24 15:39:54 -0300

    feat: add tests

--- packages/config/src/configCreator.ts
+++ packages/config/src/configCreator.ts
@@ -1,16 +1,21 @@
 import deepmerge from 'deepmerge';
 
-const overwriteMerge = (_: any, sourceArray: any) => sourceArray;
+const overwriteMerge = (_: any, sourceArray: any) => {
+  return sourceArray;
+};
 
-export const configCreator =
-  <T extends Record<string, any>>(defaultConfig: T = {} as T) =>
-  (
+export const configCreator = <T extends Record<string, any>>(
+  defaultConfig: T = {} as T
+) => {
+  return (
     config: T = {} as T,
     deepmergeConfig?: { arrayMerge: 'append' | 'overwrite' }
-  ) =>
-    deepmerge<T>(defaultConfig, config, {
+  ) => {
+    return deepmerge<T>(defaultConfig, config, {
       arrayMerge:
         deepmergeConfig?.arrayMerge === 'overwrite'
           ? overwriteMerge
           : undefined,
     });
+  };
+};
--- packages/config/src/jest.ts
+++ packages/config/src/jest.ts
@@ -1,4 +1,5 @@
 import { configCreator } from './configCreator';
+// import type { Config } from 'jest';
 
 /*
  * For a detailed explanation regarding each configuration property and type check, visit:
@@ -22,6 +23,20 @@ export const defaultConfig = {
      */
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
+  projects: [
+    {
+      displayName: 'unit',
+      testMatch: ['<rootDir>/tests/unit/**/*.test.ts'],
+    },
+    {
+      displayName: 'integration',
+      testMatch: ['<rootDir>/tests/integration/**/*.test.ts'],
+    },
+    {
+      displayName: 'e2e',
+      testMatch: ['<rootDir>/tests/e2e/**/*.test.ts'],
+    },
+  ],
 };
 
 export const jestConfig = configCreator<any>(defaultConfig);
