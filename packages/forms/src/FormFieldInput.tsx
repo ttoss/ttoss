2b271d0d6ab0986ba47f7a58dc6710bfc8958e3d
RayzaOliveira <rayza.ocr@gmail.com>
2023-02-27 11:12:05 -0300

    feat: added new props to FormFieldInput

--- packages/forms/src/FormFieldInput.tsx
+++ packages/forms/src/FormFieldInput.tsx
@@ -1,6 +1,7 @@
-import { Box, Input, type InputProps, Label } from '@ttoss/ui';
+import { Box, Flex, Input, type InputProps, Label, Text } from '@ttoss/ui';
 import { ErrorMessage } from './ErrorMessage';
 import { FieldPath, FieldValues, useController } from 'react-hook-form';
+import React from 'react';
 
 export const FormFieldInput = <
   TFieldValues extends FieldValues = FieldValues,
@@ -8,36 +9,116 @@ export const FormFieldInput = <
 >({
   label,
   name,
+  tooltipIcon,
+  trailingIcon,
+  leadingIcon,
+  showCharacterCounter,
+  sx,
   ...inputProps
 }: {
   label?: string;
   name: TName;
+  leadingIcon?: React.ReactNode;
+  tooltipIcon?: React.ReactNode;
+  trailingIcon?: React.ReactNode;
+  showCharacterCounter?: boolean;
 } & InputProps) => {
   const {
     field: { onChange, onBlur, value, ref },
+    formState: { errors },
   } = useController<any>({
     name,
     defaultValue: '',
   });
+
+  const characterCounter = React.useMemo(() => {
+    if (!value) {
+      return 0;
+    }
+
+    return value.length;
+  }, [value]);
+
+  const hasError = !!errors[name]?.message;
 
   const id = `form-field-input-${name}`;
 
   return (
     <Box>
       {label && (
-        <Label aria-disabled={inputProps.disabled} htmlFor={id}>
+        <Label
+          sx={{ display: 'flex', alignItems: 'center' }}
+          aria-disabled={inputProps.disabled}
+          htmlFor={id}
+        >
           {label}
+          {tooltipIcon && (
+            <Text
+              sx={{ marginLeft: '9px', fontSize: '12px', lineHeight: 0 }}
+              variant="tooltip-icon"
+            >
+              {tooltipIcon}
+            </Text>
+          )}
+
+          {showCharacterCounter && (
+            <Text
+              sx={{ marginLeft: 'auto', fontSize: '12px', lineHeight: 0 }}
+              variant="character-counter"
+            >
+              {characterCounter}
+            </Text>
+          )}
         </Label>
       )}
-      <Input
-        ref={ref}
-        onChange={onChange}
-        onBlur={onBlur}
-        value={value}
-        name={name}
-        id={id}
-        {...inputProps}
-      />
+      <Flex sx={{ position: 'relative' }}>
+        {leadingIcon && (
+          <Text
+            sx={{
+              position: 'absolute',
+              alignSelf: 'center',
+              fontSize: '18px',
+              left: '16px',
+              lineHeight: 0,
+            }}
+            variant="leading-icon"
+          >
+            {leadingIcon}
+          </Text>
+        )}
+        <Input
+          ref={ref}
+          onChange={onChange}
+          onBlur={onBlur}
+          value={value}
+          name={name}
+          id={id}
+          sx={{
+            paddingLeft: leadingIcon ? '50px' : undefined,
+            paddingRight: trailingIcon ? '50px' : undefined,
+            borderColor: hasError ? 'danger' : undefined,
+            borderWidth: '2px',
+            ...sx,
+          }}
+          {...inputProps}
+        />
+
+        {trailingIcon && (
+          <Text
+            sx={{
+              position: 'absolute',
+              right: '16px',
+              alignSelf: 'center',
+              fontSize: '18px',
+              lineHeight: 0,
+              color: hasError ? 'danger' : undefined,
+            }}
+            variant="trailing-icon"
+          >
+            {trailingIcon}
+          </Text>
+        )}
+      </Flex>
       <ErrorMessage name={name} />
     </Box>
   );
