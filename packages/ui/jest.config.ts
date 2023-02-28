b9a0c1f619b638ff8f76980a1ae4712d50ba51a4
RayzaOliveira <rayza.ocr@gmail.com>
2023-02-28 16:19:11 -0300

    chore: added new config to import icons from @iconify-icons

--- packages/ui/jest.config.ts
+++ packages/ui/jest.config.ts
@@ -3,6 +3,7 @@ import { jestConfig } from '@ttoss/config';
 const config = jestConfig({
   setupFilesAfterEnv: ['./tests/setupTests.tsx'],
   testEnvironment: 'jsdom',
+  transformIgnorePatterns: ['/node_modules/(?!(@iconify-icons/.*))'],
 });
 
 export default config;
