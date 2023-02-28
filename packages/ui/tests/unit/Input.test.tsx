85c879de33e6820d08fc727c101911e98f788e7a
RayzaOliveira <rayza.ocr@gmail.com>
2023-02-28 15:55:49 -0300

    test: created tests to Input component

---
+++ packages/ui/tests/unit/Input.test.tsx
@@ -0,0 +1,63 @@
+import { Icon, Input } from '../../src';
+import { render, screen } from '@ttoss/test-utils';
+
+// import alertIcon from '@iconify-icons/mdi-light/alert';
+
+test('should render Input component without icon', () => {
+  render(<Input placeholder="My Input" />);
+
+  const inputElement = screen.getByPlaceholderText('My Input');
+
+  expect(inputElement).toBeInTheDocument();
+});
+
+test('should render Input component with trailingIcon and leadingIcon as string', () => {
+  const trailingIconAsString = 'ant-design:down-square-filled';
+  const leadingIconAsString = 'ant-design:down-square-filled';
+
+  render(
+    <Input
+      placeholder="My Input"
+      trailingIcon={trailingIconAsString}
+      leadingIcon={leadingIconAsString}
+    />
+  );
+
+  const [trailingIconEl, leadingIconEl] = screen.getAllByTestId('iconify-icon');
+
+  expect(trailingIconEl).toBeInTheDocument();
+  expect(leadingIconEl).toBeInTheDocument();
+});
+
+test('should render Input component with trailingIcon and leadingIcon as Component', () => {
+  const trailingIconAsComponent = <Icon icon="ant-design:down-square-filled" />;
+  const leadingIconAsComponent = <Icon icon="ant-design:down-square-filled" />;
+
+  render(
+    <Input
+      placeholder="My Input"
+      trailingIcon={trailingIconAsComponent}
+      leadingIcon={leadingIconAsComponent}
+    />
+  );
+
+  const [trailingIconEl, leadingIconEl] = screen.getAllByTestId('iconify-icon');
+
+  expect(trailingIconEl).toBeInTheDocument();
+  expect(leadingIconEl).toBeInTheDocument();
+});
+
+// test('should render Input component with trailingIcon and leadingIcon as svg icon', () => {
+//   render(
+//     <Input
+//       placeholder="My Input"
+//       trailingIcon={alertIcon}
+//       leadingIcon={alertIcon}
+//     />
+//   );
+
+//   const [trailingIconEl, leadingIconEl] = screen.getAllByTestId('iconify-icon');
+
+//   expect(trailingIconEl).toBeInTheDocument();
+//   expect(leadingIconEl).toBeInTheDocument();
+// });
