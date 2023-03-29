ca57cae59e839f61876267c34d5a83d778ac9306
RayzaOliveira <rayza.ocr@gmail.com>
2023-03-29 17:36:33 -0300

    docs: created Badge by variant

--- docs/storybook/stories/ui/Badge.stories.tsx
+++ docs/storybook/stories/ui/Badge.stories.tsx
@@ -1,4 +1,4 @@
-import { Badge, Flex } from '@ttoss/ui';
+import { Badge, BadgeProps, Flex } from '@ttoss/ui';
 import { ComponentMeta, Story } from '@storybook/react';
 
 export default {
@@ -6,16 +6,45 @@ export default {
   component: Badge,
 } as ComponentMeta<typeof Badge>;
 
-const Template: Story = (args) => {
+const Template: Story<BadgeProps> = (args) => {
   return (
     <Flex
       sx={{
         gap: 'md',
+        flexDirection: 'column',
+        alignItems: 'start',
       }}
     >
-      <Badge {...args}>Primary</Badge>
+      <Badge {...args} />
+      <Badge {...args} icon="mdi-light:home" />
     </Flex>
   );
 };
 
-export const Example = Template.bind({});
+export const Positive = Template.bind({});
+
+Positive.args = {
+  variant: 'positive',
+  children: <>{'positive'}</>,
+};
+
+export const Negative = Template.bind({});
+
+Negative.args = {
+  variant: 'negative',
+  children: <>{'negative'}</>,
+};
+
+export const Neutral = Template.bind({});
+
+Neutral.args = {
+  variant: 'neutral',
+  children: <>{'neutral'}</>,
+};
+
+export const Informative = Template.bind({});
+
+Informative.args = {
+  variant: 'informative',
+  children: <>{'informative'}</>,
+};
