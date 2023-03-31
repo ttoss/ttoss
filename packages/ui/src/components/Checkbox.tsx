528ff8c0d424cc6195741619b82bc545e37e47d2
RayzaOliveira <rayza.ocr@gmail.com>
2023-03-31 14:35:38 -0300

    feat: created Checkbox component

--- packages/ui/src/components/Checkbox.tsx
+++ packages/ui/src/components/Checkbox.tsx
@@ -1,1 +1,23 @@
-export { Checkbox, type CheckboxProps } from 'theme-ui';
+import {
+  type CheckboxProps as CheckboxPropsUi,
+  Checkbox as CheckboxUi,
+} from 'theme-ui';
+import React from 'react';
+
+export type CheckboxProps = CheckboxPropsUi & {
+  error?: boolean;
+};
+
+export const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ error, ...props }, ref) => {
+    return (
+      <CheckboxUi
+        aria-invalid={error ? 'true' : undefined}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+Checkbox.displayName = 'Checkbox';
