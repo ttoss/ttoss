9948c5decc020a289e0bdd28e6936e4682868c57
RayzaOliveira <rayza.ocr@gmail.com>
2023-04-03 16:31:48 -0300

    docs: added new examples to Input component on stories

--- docs/storybook/stories/ui/Input.stories.tsx
+++ docs/storybook/stories/ui/Input.stories.tsx
@@ -1,4 +1,4 @@
-import { Input, InputProps } from '@ttoss/ui';
+import { Flex, Input, InputProps } from '@ttoss/ui';
 import { Meta, Story } from '@storybook/react';
 import alertIcon from '@iconify-icons/mdi-light/alert';
 
@@ -9,6 +9,19 @@ export default {
 
 const Template: Story<InputProps> = (args) => {
   return <Input {...args} />;
+};
+
+export const Default = Template.bind({});
+
+Default.args = {
+  placeholder: 'Placeholder text',
+};
+
+export const Disabled = Template.bind({});
+
+Disabled.args = {
+  disabled: true,
+  placeholder: 'Placeholder text',
 };
 
 export const IconsAsString = Template.bind({});
@@ -23,4 +36,27 @@ IconsAsSvgIcon.args = {
   trailingIcon: alertIcon,
   leadingIcon: alertIcon,
   placeholder: 'Icon as SvgIcon',
+};
+
+export const WithError = Template.bind({});
+WithError.args = {
+  trailingIcon: 'carbon:error-filled',
+  leadingIcon: alertIcon,
+  placeholder: 'Error',
+  className: 'error',
+};
+
+export const InsideFlex = () => {
+  return (
+    <Flex
+      sx={{
+        height: '100px',
+        width: '100%',
+        border: 'default',
+        padding: 'lg',
+      }}
+    >
+      <Input placeholder="Input inside container with padding" />
+    </Flex>
+  );
 };
