5f3461a108be8e92e3cf83fc6c0dc2b18a37956d
RayzaOliveira <rayza.ocr@gmail.com>
2023-04-06 16:15:53 -0300

    feat: added new props to ErrorMessage and FormFieldInput component

--- packages/forms/src/FormFieldInput.tsx
+++ packages/forms/src/FormFieldInput.tsx
@@ -1,15 +1,6 @@
-import {
-  Box,
-  Icon,
-  type IconType,
-  Input,
-  type InputProps,
-  Label,
-  Text,
-} from '@ttoss/ui';
+import { Box, Input, type InputProps, Label, type LabelProps } from '@ttoss/ui';
 import { ErrorMessage } from './ErrorMessage';
 import { FieldPath, FieldValues, useController } from 'react-hook-form';
-import React from 'react';
 
 export const FormFieldInput = <
   TFieldValues extends FieldValues = FieldValues,
@@ -17,15 +8,14 @@ export const FormFieldInput = <
 >({
   label,
   name,
-  tooltipIcon,
-  showCharacterCounter,
+  tooltip,
+  onTooltipClick,
   ...inputProps
 }: {
   label?: string;
   name: TName;
-  tooltipIcon?: IconType;
-  showCharacterCounter?: boolean;
-} & InputProps) => {
+} & InputProps &
+  Pick<LabelProps, 'tooltip' | 'onTooltipClick'>) => {
   const {
     field: { onChange, onBlur, value, ref },
     formState: { errors },
@@ -33,14 +23,6 @@ export const FormFieldInput = <
     name,
     defaultValue: '',
   });
-
-  const characterCounter = React.useMemo(() => {
-    if (!value) {
-      return 0;
-    }
-
-    return String(value).length;
-  }, [value]);
 
   const id = `form-field-input-${name}`;
 
@@ -50,33 +32,12 @@ export const FormFieldInput = <
     <Box>
       {label && (
         <Label
-          sx={{ display: 'flex', alignItems: 'center' }}
           aria-disabled={inputProps.disabled}
           htmlFor={id}
+          tooltip={tooltip}
+          onTooltipClick={onTooltipClick}
         >
           {label}
-          {tooltipIcon && (
-            <Text
-              sx={{ marginLeft: 'md', fontSize: 'xs', lineHeight: 0 }}
-              variant="tooltip-icon"
-            >
-              <Icon icon={tooltipIcon} />
-            </Text>
-          )}
-
-          {showCharacterCounter && (
-            <Text
-              sx={{
-                marginLeft: 'auto',
-                fontSize: 'xs',
-                lineHeight: 0,
-                color: 'underemphasize',
-              }}
-              variant="character-counter"
-            >
-              {characterCounter}
-            </Text>
-          )}
         </Label>
       )}
 
