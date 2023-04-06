c19bfe3e3f009e12402ed07a777c7893702686c4
Rayza Oliveira <52584988+RayzaOliveira@users.noreply.github.com>
2023-04-06 08:10:49 -0300

    Task 506 rayza modules   update label (#265)
    * feat: created Label component
    
    * style: added style to Label component in the BruttalTheme
    
    * docs: created story to Label component
    
    * test: created tests to Label component

--- packages/ui/src/components/Label.tsx
+++ packages/ui/src/components/Label.tsx
@@ -1,1 +1,32 @@
-export { Label, type LabelProps } from 'theme-ui';
+import { Icon, Text } from '..';
+import { type LabelProps as LabelPropsUi, Label as LabelUi } from 'theme-ui';
+
+const TOOLTIP_LABEL = 'tooltip';
+
+export type LabelProps = LabelPropsUi & {
+  tooltip?: boolean;
+  onTooltipClick?: () => void;
+};
+
+export const Label = ({
+  children,
+  onTooltipClick,
+  tooltip,
+  ...props
+}: LabelProps) => {
+  return (
+    <LabelUi {...props}>
+      {children}
+
+      {tooltip && (
+        <Text
+          sx={{ color: 'currentcolor' }}
+          onClick={onTooltipClick}
+          aria-label={TOOLTIP_LABEL}
+        >
+          <Icon inline icon="info" />
+        </Text>
+      )}
+    </LabelUi>
+  );
+};
