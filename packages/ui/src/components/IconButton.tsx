cb5a926bfdf66fbbe924e03f62153aabae8891c7
Pedro Arantes <arantespp@gmail.com>
2023-04-12 13:05:30 -0300

    fix: set icon button type as button by default (#268)

--- packages/ui/src/components/IconButton.tsx
+++ packages/ui/src/components/IconButton.tsx
@@ -1,1 +1,12 @@
-export { IconButton, type IconButtonProps } from 'theme-ui';
+import * as React from 'react';
+import { type IconButtonProps, IconButton as IconButtonUi } from 'theme-ui';
+
+export type { IconButtonProps };
+
+export const IconButton = React.forwardRef<HTMLButtonElement, IconButtonProps>(
+  (props, ref) => {
+    return <IconButtonUi type="button" {...props} ref={ref} />;
+  }
+);
+
+IconButton.displayName = 'IconButton';
