0e0926e7216dada779d64adddce8d212e14967e7
RayzaOliveira <rayza.ocr@gmail.com>
2023-04-12 10:35:16 -0300

    docs: created story to FormGroup component

--- docs/storybook/stories/forms/FormGroup.stories.tsx
+++ docs/storybook/stories/forms/FormGroup.stories.tsx
@@ -17,12 +17,36 @@ const Template: Story = () => {
   return (
     <Form {...formMethods} onSubmit={action('onSubmit')}>
       <FormGroup>
-        <FormFieldInput name="input1" label="input1" />
-        <FormFieldInput name="input2" label="input2" />
+        <FormFieldInput name="input1" placeholder="input1" label="input1" />
+        <FormFieldInput name="input2" placeholder="input2" label="input2" />
 
         <FormGroup>
-          <FormFieldInput name="input3" label="input3" />
-          <FormFieldInput name="input4" label="input4" />
+          <FormFieldInput name="input3" placeholder="input3" label="input3" />
+          <FormFieldInput name="input4" placeholder="input4" label="input4" />
+
+          <FormGroup>
+            <FormFieldInput name="input5" placeholder="input5" label="input5" />
+
+            <FormGroup>
+              <FormFieldInput
+                name="input6"
+                placeholder="input6"
+                label="input6"
+              />
+              <FormGroup>
+                <FormFieldInput
+                  name="input9"
+                  placeholder="input9"
+                  label="input9"
+                />
+              </FormGroup>
+            </FormGroup>
+          </FormGroup>
+
+          <FormGroup>
+            <FormFieldInput name="input7" placeholder="input7" label="input7" />
+            <FormFieldInput name="input8" placeholder="input8" label="input8" />
+          </FormGroup>
         </FormGroup>
       </FormGroup>
 
