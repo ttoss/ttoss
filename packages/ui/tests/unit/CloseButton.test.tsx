c9d8b7bd97ea2ba9206e56c0cee83a126ef813d5
RayzaOliveira <rayza.ocr@gmail.com>
2023-04-12 18:20:10 -0300

    test: added tests to CloseButton component

---
+++ packages/ui/tests/unit/CloseButton.test.tsx
@@ -0,0 +1,25 @@
+import { CloseButton } from '../../src';
+import { render, screen, userEvent } from '@ttoss/test-utils';
+
+test('should render CloseButton without label', () => {
+  render(<CloseButton />);
+  expect(screen.getByTestId('iconify-icon')).toBeInTheDocument();
+  expect(screen.getByTestId('iconify-icon')).toHaveAttribute('icon', 'close');
+});
+
+test('should render CloseButton with label', () => {
+  const LABEL = 'Close Modal';
+  render(<CloseButton label={LABEL} />);
+  expect(screen.getByText(LABEL)).toBeInTheDocument();
+});
+
+test('should call onClick function', async () => {
+  const LABEL = 'Close Modal';
+  const user = userEvent.setup({ delay: null });
+  const onClick = jest.fn();
+
+  render(<CloseButton label={LABEL} onClick={onClick} />);
+
+  await user.click(screen.getByText(LABEL));
+  expect(onClick).toHaveBeenCalled();
+});
