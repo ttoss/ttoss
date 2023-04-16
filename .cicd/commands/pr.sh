3c1ed84d07533c5554afd0e79d290d66ff04c097
Pedro Arantes <arantespp@gmail.com>
2023-04-16 17:55:10 -0300

    fix: pr command

--- .cicd/commands/pr.sh
+++ .cicd/commands/pr.sh
@@ -1,4 +1,4 @@
 # Test, build, and deploy all packages since main
 # and all the workspaces that depends on them.
 # https://turbo.build/repo/docs/core-concepts/monorepos/filtering#include-dependents-of-matched-workspaces
-pnpm turbo run build test deploy --filter=[main]
+pnpm turbo run build test deploy --filter=...[main]
