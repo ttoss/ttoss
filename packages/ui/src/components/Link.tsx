3a77ad387b6d98927bc992fa9a05fd4bb564744c
RayzaOliveira <rayza.ocr@gmail.com>
2023-04-18 18:02:05 -0300

    feat: created Link component

--- packages/ui/src/components/Link.tsx
+++ packages/ui/src/components/Link.tsx
@@ -1,1 +1,20 @@
-export { Link, type LinkProps } from 'theme-ui';
+import { type LinkProps as LinkPropsUi, Link as LinkUi } from 'theme-ui';
+import React from 'react';
+
+export type LinkProps = LinkPropsUi & {
+  quiet?: boolean;
+};
+
+export const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(
+  ({ quiet, className, ...props }, ref) => {
+    return (
+      <LinkUi
+        className={`${quiet ? 'quiet' : ''} ${className ?? ''}`}
+        {...props}
+        ref={ref}
+      />
+    );
+  }
+);
+
+Link.displayName = 'Link';
