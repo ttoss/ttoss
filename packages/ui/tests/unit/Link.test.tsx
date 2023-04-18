6b79fa5afb18c961770311311344a15105aabcd7
RayzaOliveira <rayza.ocr@gmail.com>
2023-04-18 18:11:51 -0300

    test: created test to Link component

---
+++ packages/ui/tests/unit/Link.test.tsx
@@ -0,0 +1,13 @@
+import { Link } from '../../src';
+import { render, screen } from '@ttoss/test-utils';
+
+const CONTENT_LINK = 'Link Text';
+
+test('should render Link with quiet class', () => {
+  render(<Link quiet>{CONTENT_LINK}</Link>);
+
+  const linkEl = screen.getByText(CONTENT_LINK);
+
+  expect(linkEl).toBeInTheDocument();
+  expect(linkEl).toHaveClass('quiet');
+});
