8718ac3c42cd20d75ab5b0f5d0e1ab0c44834c9d
RayzaOliveira <rayza.ocr@gmail.com>
2023-04-19 13:39:28 -0300

    docs: created docs to Radio component

---
+++ docs/storybook/stories/ui/Radio.stories.tsx
@@ -0,0 +1,42 @@
+import { Label, Radio, RadioProps } from '@ttoss/ui/src';
+import { Meta, StoryObj } from '@storybook/react';
+
+type Story = StoryObj<typeof Radio>;
+
+const Component = (args: RadioProps) => {
+  return (
+    <Label>
+      <Radio {...args} />
+      Label text
+    </Label>
+  );
+};
+
+const meta: Meta<typeof Radio> = {
+  title: 'UI/Radio',
+  component: Component,
+};
+
+export default meta;
+
+export const DefaultUnchecked: Story = {
+  args: {},
+};
+
+export const DefaultChecked: Story = {
+  args: {
+    defaultChecked: true,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
+
+export const Error: Story = {
+  args: {
+    'aria-invalid': 'true',
+  },
+};
