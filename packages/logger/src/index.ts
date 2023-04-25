56ebcca428475c2e4e8c8d12939b25070e54f4a5
Eron Alves <eron.alves@rocketmail.com>
2023-04-25 01:35:15 -0300

    feat: increment logger info

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,18 +1,23 @@
 const { log, warn, error } = console;
 const isDev: boolean = (import.meta as any).env.DEV || (process.env as any).DEV;
 
-export const logger = {
-  warn: (value: string) => {
-    if (isDev) {
-      warn(value);
-    }
-  },
-  info: (value: string) => {
-    if (isDev) {
-      log(value);
-    }
-  },
-  error: (value: string) => {
-    error(value);
-  },
+export const Logger = (prefix: string) => {
+  return {
+    warn: (value: string) => {
+      if (isDev) {
+        const now = Date.now();
+        warn(`[${now}] - ${prefix} - ${value}`);
+      }
+    },
+    info: (value: string) => {
+      if (isDev) {
+        const now = Date.now();
+        log(`[${now}] - ${prefix} - ${value}`);
+      }
+    },
+    error: (value: string) => {
+      const now = Date.now();
+      error(`[${now}] - ${prefix} - ${value}`);
+    },
+  };
 };
