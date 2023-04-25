06a32108795d8ebfc90a688114f301f6ce7c57c2
Eron Alves <eron.alves@rocketmail.com>
2023-04-25 16:37:11 -0300

    fix: expose isDev config

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,27 +1,24 @@
 const { log, warn, error } = console;
 
-const nodeEnv = process?.env as any;
-const browserEnv = (import.meta as any)?.env;
-
-const isDev: boolean = browserEnv?.DEV === 'true' || nodeEnv?.DEV === 'true';
-
-export const Logger = (prefix: string) => {
-  return {
-    warn: (value: string) => {
-      if (isDev) {
+export const Logger = (isDev: boolean) => {
+  return (prefix: string) => {
+    return {
+      warn: (value: string) => {
+        if (isDev) {
+          const now = new Date();
+          warn(`[${now}] - ${prefix} - ${value}`);
+        }
+      },
+      info: (value: string) => {
+        if (isDev) {
+          const now = new Date();
+          log(`[${now}] - ${prefix} - ${value}`);
+        }
+      },
+      error: (value: string) => {
         const now = new Date();
-        warn(`[${now}] - ${prefix} - ${value}`);
-      }
-    },
-    info: (value: string) => {
-      if (isDev) {
-        const now = new Date();
-        log(`[${now}] - ${prefix} - ${value}`);
-      }
-    },
-    error: (value: string) => {
-      const now = new Date();
-      error(`[${now}] - ${prefix} - ${value}`);
-    },
+        error(`[${now}] - ${prefix} - ${value}`);
+      },
+    };
   };
 };
