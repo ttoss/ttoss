dd0fe77dcb032aae1663443726fc09dc052440bd
Eron Alves <eron.alves@rocketmail.com>
2023-04-25 16:59:23 -0300

    feat: expose isDev option

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,16 +1,24 @@
 const { log, warn, error } = console;
 
-export const Logger = (isDev: boolean) => {
+export const Logger = (isDev?: boolean) => {
+  let devEnv = true;
+
+  if (isDev === undefined) {
+    // eslint-disable-next-line turbo/no-undeclared-env-vars
+    const node_env = process?.env?.NODE_ENV;
+    if (node_env === 'production') devEnv = false;
+  } else devEnv = isDev;
+
   return (prefix: string) => {
     return {
       warn: (value: string) => {
-        if (isDev) {
+        if (devEnv) {
           const now = new Date();
           warn(`[${now}] - ${prefix} - ${value}`);
         }
       },
       info: (value: string) => {
-        if (isDev) {
+        if (devEnv) {
           const now = new Date();
           log(`[${now}] - ${prefix} - ${value}`);
         }
