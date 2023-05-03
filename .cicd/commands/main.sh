de682a05d4774a99715d46e72eea29b678964c28
Pedro Arantes <arantespp@gmail.com>
2023-05-03 18:37:59 -0300

    ci: update main commands

--- .cicd/commands/main.sh
+++ .cicd/commands/main.sh
@@ -12,7 +12,7 @@ git fetch --tags --quiet
 # which indicates success.
 git tag --points-at HEAD | grep -q . && { echo "There are tags in the current commit, exiting main workflow" && exit 0; }
 
-
+# Retrieve the latest tag
 LATEST_TAG=$(git describe --tags --abbrev=0)
 
 # Setup NPM token
@@ -29,45 +29,55 @@ echo NPM whoami: $(npm whoami)
 # not works because of package version and "workspace:^" mismatch.
 pnpm turbo run build:config
 
-# Version before publish to rebuild all packages that Lerna will publish
-pnpm lerna version --yes --no-push
+# Publish packages only if `pnpm lerna changed` is success. This happens when
+# exists an updade on root and no packages changes. This way, `version` won't
+# create tags and `git diff HEAD^1 origin/main --quiet` will fail because
+# HEAD^1 will diff from origin/main.
+if pnpm lerna changed; then
+  echo "Changes detected on packages, publishing them..."
+  
+  # Version before publish to rebuild all packages that Lerna will publish
+  pnpm lerna version --yes --no-push
 
-# Test and build all packages since $LATEST_TAG
-# and all the workspaces that depends on them
-# https://turbo.build/repo/docs/core-concepts/monorepos/filtering#include-dependents-of-matched-workspaces
-pnpm turbo run build test --filter=...[$LATEST_TAG]
+  # Test and build all packages since $LATEST_TAG
+  # and all the workspaces that depends on them
+  # https://turbo.build/repo/docs/core-concepts/monorepos/filtering#include-dependents-of-matched-workspaces
+  pnpm turbo run build test --filter=...[$LATEST_TAG]
 
-# Undo all files that were changed by the build command—this happens because
-# the build can change files with different linting rules, or modify some
-# auto-generated docs. We don't want these changes becaues it will cause
-# turbo cache missing. https://turbo.build/repo/docs/core-concepts/caching#missing-the-cache
-git checkout -- .
+  # Undo all files that were changed by the build command—this happens because
+  # the build can change files with different linting rules, or modify some
+  # auto-generated docs. We don't want these changes becaues it will cause
+  # turbo cache missing. https://turbo.build/repo/docs/core-concepts/caching#missing-the-cache
+  git checkout -- .
 
-# Use Git to check for changes in the origin repository. If there are any
-# changes, "git push --follow-tags" will fail. The error message will be:
-#
-# error: failed to push some refs to 'github.com:ttoss/ttoss.git'
-# hint: Updates were rejected because the remote contains work that you do
-# hint: not have locally. This is usually caused by another repository pushing
-# hint: to the same ref. You may want to first integrate the remote changes
-# hint: (e.g., 'git pull ...') before pushing again.
-#
-# To avoid this, we need to:
-#
-# 1. Fetch the latest changes from the origin/main repository.
-# 2. Compare the local and remote main branches using `git diff`.
-# 3. Check if there are any changes and stop the workflow if there are any.
-# 4. Exit and wait to the next main workflow starts because of the changes.
-git fetch
+  # Use Git to check for changes in the origin repository. If there are any
+  # changes, "git push --follow-tags" will fail. The error message will be:
+  #
+  # error: failed to push some refs to 'github.com:ttoss/ttoss.git'
+  # hint: Updates were rejected because the remote contains work that you do
+  # hint: not have locally. This is usually caused by another repository pushing
+  # hint: to the same ref. You may want to first integrate the remote changes
+  # hint: (e.g., 'git pull ...') before pushing again.
+  #
+  # To avoid this, we need to:
+  #
+  # 1. Fetch the latest changes from the origin/main repository.
+  # 2. Compare the local and remote main branches using `git diff`.
+  # 3. Check if there are any changes and stop the workflow if there are any.
+  # 4. Exit and wait to the next main workflow starts because of the changes.
+  git fetch
 
-# HEAD^1 because lerna version created a commit
-git diff HEAD^1 origin/main --quiet || { echo "Changes found before publishing. Workflow stopped." && exit 1; }
+  # HEAD^1 because lerna version created a commit
+  git diff HEAD^1 origin/main --quiet || { echo "Changes found before publishing. Workflow stopped." && exit 1; }
 
-# Push changes
-git push --follow-tags
+  # Push changes
+  git push --follow-tags
 
-# Publish packages
-pnpm lerna publish from-git --yes
+  # Publish packages
+  pnpm lerna publish from-git --yes
+else
+  echo "No changes detected on packages, skipping publish..."
+fi
 
 # Deploy after publish because there are cases in which a package is versioned
 # and it should be on NPM registry to Lambda Layer create the new version when
