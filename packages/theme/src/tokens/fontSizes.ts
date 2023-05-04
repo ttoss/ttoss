0c65849accc271362838905abf45421945fdb4bf
RayzaOliveira <rayza.ocr@gmail.com>
2023-05-04 17:03:14 -0300

    fix: fixed fonts and spaces using new built functions

--- packages/theme/src/tokens/fontSizes.ts
+++ packages/theme/src/tokens/fontSizes.ts
@@ -1,17 +1,21 @@
-const BASE = '1rem';
+import { calcMultiplyingValues } from '../helpers/geometricCalc';
+
+const SIZE_TYPE = 'rem';
+
+const BASE = 1;
 
 const MULTIPLIER_FACTOR = 1.333;
 
 export const fontSizes = {
-  '2xs': `calc(${BASE} * (${Math.pow(MULTIPLIER_FACTOR, -3)})`,
-  xs: `calc(${BASE} * ${Math.pow(MULTIPLIER_FACTOR, -2)})`,
-  sm: `calc(${BASE} * ${Math.pow(MULTIPLIER_FACTOR, -1)})`,
-  base: BASE,
-  lg: `calc(${BASE} * (${MULTIPLIER_FACTOR}^1))`,
-  xl: `calc(${BASE} * (${MULTIPLIER_FACTOR}^2))`,
-  '2xl': `calc(${BASE} * (${MULTIPLIER_FACTOR}^3))`,
-  '3xl': `calc(${BASE} * (${MULTIPLIER_FACTOR}^4))`,
-  '4xl': `calc(${BASE} * (${MULTIPLIER_FACTOR}^5))`,
-  '5xl': `calc(${BASE} * (${MULTIPLIER_FACTOR}^6))`,
-  '6xl': `calc(${BASE} * (${MULTIPLIER_FACTOR}^7))`,
+  '2xs': calcMultiplyingValues(BASE, MULTIPLIER_FACTOR, -3),
+  xs: calcMultiplyingValues(BASE, MULTIPLIER_FACTOR, -2),
+  sm: calcMultiplyingValues(BASE, MULTIPLIER_FACTOR, -1),
+  base: `${BASE}${SIZE_TYPE}`,
+  lg: calcMultiplyingValues(BASE, MULTIPLIER_FACTOR, 1),
+  xl: calcMultiplyingValues(BASE, MULTIPLIER_FACTOR, 2),
+  '2xl': calcMultiplyingValues(BASE, MULTIPLIER_FACTOR, 3),
+  '3xl': calcMultiplyingValues(BASE, MULTIPLIER_FACTOR, 4),
+  '4xl': calcMultiplyingValues(BASE, MULTIPLIER_FACTOR, 5),
+  '5xl': calcMultiplyingValues(BASE, MULTIPLIER_FACTOR, 6),
+  '6xl': calcMultiplyingValues(BASE, MULTIPLIER_FACTOR, 7),
 };
