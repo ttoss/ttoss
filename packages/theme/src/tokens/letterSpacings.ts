fd59de6a5e6c5865bab8664d97a1564a4a8c7cae
RayzaOliveira <rayza.ocr@gmail.com>
2023-05-08 18:08:46 -0300

    feat: updated letterSpacing values to match Figma

--- packages/theme/src/tokens/letterSpacings.ts
+++ packages/theme/src/tokens/letterSpacings.ts
@@ -1,8 +1,8 @@
 export const letterSpacings = {
   tighter: '-0.05em',
   tight: '-0.025em',
-  normal: '0em',
-  wide: '0.025em',
-  wider: '0.05em',
-  widest: '0.1em',
+  regular: '0em',
+  wide: '0.05em',
+  wider: '0.1em',
+  widest: '0.5em',
 };
