72a6cfe60e4542db8040dddc3e37f4f04234a97d
Pedro Arantes <arantespp@gmail.com>
2023-05-08 17:19:28 -0300

    fix: pr main command

--- .cicd/commands/pr.sh
+++ .cicd/commands/pr.sh
@@ -1,7 +1,13 @@
 # Retrieve the latest tag
 export LATEST_TAG=$(git describe --tags --abbrev=0)
 
-# Test, build, and deploy all packages since main
+# Test and build all packages since main
 # and all the workspaces that depends on them.
 # https://turbo.build/repo/docs/core-concepts/monorepos/filtering#include-dependents-of-matched-workspaces
-pnpm turbo run build test deploy --filter=...[main]
+pnpm turbo run build test --filter=...[main]
+
+# Run deploy separately from command above because we don't want to deploy
+# packages with bug. As `test` isn't a dependsOn of `deploy` on turbo.json,
+# we need to run them separately. If we run them together and deploy is faster,
+# `deploy` will run even if `test` fails.
+pnpm turbo run deploy --filter=...[main]
