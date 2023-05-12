1e1887ec54973111b55e403f0045b79ed750ef49
RayzaOliveira <rayza.ocr@gmail.com>
2023-05-12 10:07:43 -0300

    feat: updated FormFieldPassword component

--- packages/forms/src/FormFieldPassword.tsx
+++ packages/forms/src/FormFieldPassword.tsx
@@ -1,32 +1,67 @@
-import { FieldPath, FieldValues } from 'react-hook-form';
+import { ErrorMessage } from './ErrorMessage';
+import { FieldPath, FieldValues, useController } from 'react-hook-form';
+import {
+  Flex,
+  InputPassword,
+  type InputPasswordProps,
+  Label,
+  type LabelProps,
+} from '@ttoss/ui';
 
-import { FormFieldInput, type FormFieldInputProps } from './FormFieldInput';
-import { useHidePassInput } from './useHidePassInput';
-
-type FormFieldPasswordProps<TName> = Omit<
-  FormFieldInputProps<TName>,
-  'type' | 'trailingIcon' | 'onTrailingIconClick'
-> & {
-  showPasswordByDefault?: boolean;
-};
+export type FormFieldPasswordProps<TName> = {
+  label?: string;
+  name: TName;
+} & InputPasswordProps &
+  Pick<LabelProps, 'tooltip' | 'onTooltipClick'>;
 
 export const FormFieldPassword = <
   TFieldValues extends FieldValues = FieldValues,
   TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
 >({
-  showPasswordByDefault,
-  ...passwordProps
+  label,
+  name,
+  tooltip,
+  onTooltipClick,
+  sx,
+  ...inputProps
 }: FormFieldPasswordProps<TName>) => {
-  const { handleClick, icon, inputType } = useHidePassInput(
-    !showPasswordByDefault
-  );
+  const {
+    field: { onChange, onBlur, value, ref },
+    formState: { errors },
+  } = useController<any>({
+    name,
+    defaultValue: '',
+  });
+
+  const id = `form-field-password-${name}`;
+
+  const hasError = !!errors[name]?.message;
 
   return (
-    <FormFieldInput
-      {...passwordProps}
-      trailingIcon={icon}
-      onTrailingIconClick={handleClick}
-      type={inputType}
-    />
+    <Flex sx={{ flexDirection: 'column', width: '100%', ...sx }}>
+      {label && (
+        <Label
+          aria-disabled={inputProps.disabled}
+          htmlFor={id}
+          tooltip={tooltip}
+          onTooltipClick={onTooltipClick}
+        >
+          {label}
+        </Label>
+      )}
+
+      <InputPassword
+        ref={ref}
+        onChange={onChange}
+        className={hasError ? 'error' : ''}
+        onBlur={onBlur}
+        value={value}
+        name={name}
+        id={id}
+        {...inputProps}
+      />
+
+      <ErrorMessage name={name} />
+    </Flex>
   );
 };
