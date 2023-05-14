2da4016a3dbd9d9d5935bbb73638bee6a9a1d614
Pedro Arantes <arantespp@gmail.com>
2023-05-14 13:10:22 -0300

    chore: improve i18n workflow (#345)
    * chore: improve i18n workflow
    
    * fix: turbo build dep graph
    
    * fix: react auth compile

--- .cicd/commands/pr.sh
+++ .cicd/commands/pr.sh
@@ -1,4 +1,4 @@
-# Retrieve the latest tag
+# Retrieve the latest tag.
 export LATEST_TAG=$(git describe --tags --abbrev=0)
 
 # Test and build all packages since main
@@ -6,11 +6,9 @@ export LATEST_TAG=$(git describe --tags --abbrev=0)
 # https://turbo.build/repo/docs/core-concepts/monorepos/filtering#include-dependents-of-matched-workspaces
 pnpm turbo run build test --filter=...[main]
 
-# Undo all files that were changed by the build command—this happens because
-# the build can change files with different linting rules, or modify some
-# auto-generated docs. We don't want these changes becaues it will cause
-# turbo cache missing. https://turbo.build/repo/docs/core-concepts/caching#missing-the-cache
-git checkout -- .
+# See description on the main.sh file.
+pnpm turbo run lint
+git diff --exit-code --quiet || { echo "Error: There are changed files."; git status; exit 1; }
 
 # Run deploy separately from command above because we don't want to deploy
 # packages with bug. As `test` isn't a dependsOn of `deploy` on turbo.json,
