3516f067e1314a21baadb3761812c34f1029923c
RayzaOliveira <rayza.ocr@gmail.com>
2023-05-18 15:37:55 -0300

    feat: exported NotifyParams

--- packages/react-notifications/src/index.ts
+++ packages/react-notifications/src/index.ts
@@ -2,5 +2,6 @@ export {
   NotificationsProvider,
   useNotifications,
   NotificationsProviderProps,
+  type NotifyParams,
 } from './Provider';
 export { NotificationBox } from './NotificationBox';
