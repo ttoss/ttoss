1d8cdf0e1681f6fb76b5a82e48c861b707ca126d
Eron Alves <eron.alves@rocketmail.com>
2023-05-19 12:35:52 -0300

    fix: adjust style

--- packages/react-notifications/src/components/NotificationsModal.tsx
+++ packages/react-notifications/src/components/NotificationsModal.tsx
@@ -1,33 +1,15 @@
 import * as React from 'react';
-import { Button, Stack } from '@ttoss/ui';
 import { Modal } from '../../components/dist';
 import { NotificationsBox } from './NotificationsBox';
-import { useI18n } from '@ttoss/react-i18n';
-import { useNotifications } from '../Provider';
-import { useState } from 'react';
+import { Stack } from '@ttoss/ui';
+import { useNotifications } from './Provider';
 
 export const NotificationsModal = () => {
   const { notifications, setNotifications } = useNotifications();
-  const {
-    intl: { formatMessage },
-  } = useI18n();
 
   return (
     <Modal isOpen={!!notifications} style={{ content: { minWidth: '30%' } }}>
-      <Stack sx={{ gap: '2xl' }}>
-        <NotificationsBox />
-        <Button
-          onClick={() => {
-            setNotifications(undefined);
-          }}
-          sx={{ width: '50%' }}
-        >
-          {formatMessage({
-            defaultMessage: 'OK',
-            description: 'OK',
-          })}
-        </Button>
-      </Stack>
+      <NotificationsBox direction="stack" />
     </Modal>
   );
 };
