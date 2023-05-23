dac8498ee3c4863a1f2b5c380241869875c0a34e
Pedro Arantes <arantespp@gmail.com>
2023-05-23 16:35:52 -0300

    chore: update packages

--- terezinha-farm/app/src/amplify.ts
+++ terezinha-farm/app/src/amplify.ts
@@ -1,11 +1,10 @@
-/* eslint-disable turbo/no-undeclared-env-vars */
 import { Amplify } from 'aws-amplify';
 
 Amplify.configure({
   Auth: {
-    identityPoolId: process.env.VITE_IDENTITY_POOL_ID,
-    region: process.env.VITE_REGION,
-    userPoolId: process.env.VITE_USER_POOL_ID,
-    userPoolWebClientId: process.env.VITE_APP_CLIENT_ID,
+    identityPoolId: import.meta.env.VITE_IDENTITY_POOL_ID,
+    region: import.meta.env.VITE_REGION,
+    userPoolId: import.meta.env.VITE_USER_POOL_ID,
+    userPoolWebClientId: import.meta.env.VITE_APP_CLIENT_ID,
   },
 });
