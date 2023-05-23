3ec7c475730acaa46ff4909623930fd97c259b97
Pedro Arantes <arantespp@gmail.com>
2023-05-23 17:33:23 -0300

    fix: lint staged git error

--- .cicd/commands/pr.sh
+++ .cicd/commands/pr.sh
@@ -1,6 +1,10 @@
 # Retrieve the latest tag.
 export LATEST_TAG=$(git describe --tags --abbrev=0)
 
+# See description on the main.sh file.
+pnpm turbo run lint
+git diff --exit-code --quiet || { echo "Error: There are changed files."; git status; exit 1; }
+
 # Test and build all packages since main
 # and all the workspaces that depends on them.
 # https://turbo.build/repo/docs/core-concepts/monorepos/filtering#include-dependents-of-matched-workspaces
--- terezinha-farm/config/src/index.ts
+++ terezinha-farm/config/src/index.ts
@@ -13,5 +13,4 @@ export const config = {
   REGION: process.env.REGION,
   USER_POOL_ID: process.env.USER_POOL_ID,
   APP_CLIENT_ID: process.env.APP_CLIENT_ID,
-
 };
