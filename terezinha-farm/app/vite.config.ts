351e9853b6efce90f8bbf012098bfa5c8fd071a8
Pedro Arantes <arantespp@gmail.com>
2023-05-23 18:52:53 -0300

    fix: add dotenv to deploy app (#363)
    * chore: update packages
    
    * chore: update root packages
    
    * chore: update lint-staged
    
    * fix: lint staged git error
    
    * fix: lint staged git error
    
    * fix: lint staged git error
    
    * fix: cicd commands lint
    
    * fix: cicd commands lint
    
    * chore: change i18n to test
    
    * fix: i18n

--- terezinha-farm/app/vite.config.ts
+++ terezinha-farm/app/vite.config.ts
@@ -1,28 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 import { babelConfig } from '@ttoss/config';
-import { config } from '@terezinha-farm/config';
 import { defineConfig } from 'vite';
 import { visualizer } from 'rollup-plugin-visualizer';
 import react from '@vitejs/plugin-react';
 
-/**
- * Create an object with the same values as the config object, but with the
- * keys prefixed with `VITE_`.
- */
-const configVite = Object.entries(config).reduce((acc, [key, value]) => {
-  acc[`VITE_${key}`] = value;
-  return acc;
-}, {});
-
 // https://vitejs.dev/config/
+// eslint-disable-next-line import/no-default-export
 export default defineConfig(async () => {
   return {
-    define: {
-      'process.env': {
-        ...configVite,
-      },
-    },
     plugins: [
       react({
         babel: {
