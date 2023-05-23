a0ec3054ad56fae9116a9436f3ca59904ccd1064
Pedro Arantes <arantespp@gmail.com>
2023-05-23 18:29:10 -0300

    fix: cicd commands lint

--- .cicd/commands/pr.sh
+++ .cicd/commands/pr.sh
@@ -1,18 +1,16 @@
 # Retrieve the latest tag.
 export LATEST_TAG=$(git describe --tags --abbrev=0)
 
-# See description on the main.sh file.
-pnpm turbo run lint
-git diff --exit-code --quiet || { echo "Error: There are changed files."; git status; exit 1; }
+# See description on the lint.sh file.
+sh -e "$(dirname "$0")/lint.sh"
 
 # Test and build all packages since main
 # and all the workspaces that depends on them.
 # https://turbo.build/repo/docs/core-concepts/monorepos/filtering#include-dependents-of-matched-workspaces
 pnpm turbo run build test --filter=...[main]
 
-# See description on the main.sh file.
-pnpm turbo run lint
-git diff --exit-code --quiet || { echo "Error: There are changed files."; git status; exit 1; }
+# See description on the lint.sh file.
+sh -e "$(dirname "$0")/lint.sh"
 
 # Run deploy separately from command above because we don't want to deploy
 # packages with bug. As `test` isn't a dependsOn of `deploy` on turbo.json,
