1ae1d3c916367a282dc392831d822df3d90e66fb
Eron Alves <eron.alves@rocketmail.com>
2023-05-30 01:03:21 -0300

    test: overlay demonstration

--- docs/storybook/stories/react-notifications/Modal.stories.tsx
+++ docs/storybook/stories/react-notifications/Modal.stories.tsx
@@ -1,19 +1,34 @@
-import { Button } from '@ttoss/ui';
+import { Button, Flex } from '@ttoss/ui';
+import {
+  Dispatch,
+  SetStateAction,
+  createContext,
+  useContext,
+  useState,
+} from 'react';
 import { Meta, Story } from '@storybook/react';
+import { Modal } from '@ttoss/components/src';
 import {
   NotificationsModal,
   NotificationsProvider,
   useNotifications,
 } from '@ttoss/react-notifications/src';
 
+const ZIndexContext = createContext<Dispatch<SetStateAction<number>> | null>(
+  null
+);
+
 export default {
   title: 'React Notifications/Modal',
   decorators: [
     (Story) => {
+      const [zIndex, setZIndex] = useState(0);
       return (
         <NotificationsProvider>
-          <Story />
-          <NotificationsModal />
+          <ZIndexContext.Provider value={setZIndex}>
+            <Story />
+            <NotificationsModal {...{ zIndex }} />
+          </ZIndexContext.Provider>
         </NotificationsProvider>
       );
     },
@@ -52,5 +67,44 @@ const Template2: Story = () => {
   );
 };
 
+const Template3: Story = () => {
+  const { setNotifications } = useNotifications();
+  const [overlayModalOpen, setOverlayModalOpen] = useState(false);
+  const setZIndex = useContext(ZIndexContext);
+
+  return (
+    <>
+      <Modal isOpen={overlayModalOpen} style={{ overlay: { zIndex: 1 } }}>
+        <Flex sx={{ gap: 'lg' }}>
+          <Button
+            onClick={() => {
+              setOverlayModalOpen(false);
+            }}
+          >
+            Close
+          </Button>
+          <Button
+            onClick={() => {
+              if (!setZIndex) return null;
+              return setZIndex(2);
+            }}
+          >
+            Get Notification to Front
+          </Button>
+        </Flex>
+      </Modal>
+      <Button
+        onClick={() => {
+          setNotifications({ type: 'warning', message: 'Hello' });
+          setOverlayModalOpen(true);
+        }}
+      >
+        Click me!!
+      </Button>
+    </>
+  );
+};
+
 export const SingleNotification = Template.bind({});
 export const MultipleNotifications = Template2.bind({});
+export const WithOverlayModal = Template3.bind({});
