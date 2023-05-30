28c5639c6f0131c8a4d6b67c0a1ac005105a0872
Eron Alves <eron.alves@rocketmail.com>
2023-05-30 01:02:37 -0300

    feat: include zIndex in notification modal

--- packages/react-notifications/src/NotificationsModal.tsx
+++ packages/react-notifications/src/NotificationsModal.tsx
@@ -2,11 +2,14 @@ import { Modal } from '@ttoss/components';
 import { NotificationsBox } from './NotificationsBox';
 import { useNotifications } from './Provider';
 
-export const NotificationsModal = () => {
+export const NotificationsModal = ({ zIndex }: { zIndex?: number }) => {
   const { notifications } = useNotifications();
 
   return (
-    <Modal isOpen={!!notifications} style={{ content: { minWidth: '30%' } }}>
+    <Modal
+      isOpen={!!notifications}
+      style={{ content: { minWidth: '30%' }, overlay: { zIndex } }}
+    >
       <NotificationsBox direction="stack" />
     </Modal>
   );
