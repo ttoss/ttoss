385d613a3ee7a9816d25a3943351762e4f0a7200
RayzaOliveira <rayza.ocr@gmail.com>
2023-05-30 16:01:37 -0300

    docs: created stories to ActionButton component

---
+++ docs/storybook/stories/ui/ActionButton.stories.tsx
@@ -0,0 +1,48 @@
+import { ActionButton } from '@ttoss/ui/src';
+import { Meta, StoryObj } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+type Story = StoryObj<typeof ActionButton>;
+
+export default {
+  title: 'UI/ActionButton',
+  component: ActionButton,
+} as Meta<typeof ActionButton>;
+
+export const Default: Story = {
+  args: {
+    onClick: action('onClick'),
+    children: 'Text',
+    icon: 'radio-not-selected',
+    disabled: false,
+  },
+};
+
+export const DefaultSelected: Story = {
+  args: {
+    ...Default.args,
+    autoFocus: true,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    ...Default.args,
+    disabled: true,
+  },
+};
+
+export const Accent: Story = {
+  args: {
+    ...Default.args,
+    variant: 'accent',
+    autoFocus: true,
+  },
+};
+
+export const Quiet: Story = {
+  args: {
+    ...Default.args,
+    variant: 'quiet',
+  },
+};
