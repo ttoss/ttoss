73d9450ddb14a32ee6b3c10fd8fe44969466cad1
Eron Alves <eron.alves@rocketmail.com>
2023-05-31 18:03:37 -0300

    fix: intl provider must be defined in component ancestry

--- packages/forms/tests/setupTests.tsx
+++ packages/forms/tests/setupTests.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
+import { I18nProvider } from '@ttoss/react-i18n';
 import { ThemeProvider } from '@ttoss/ui';
 import { setOptions } from '@ttoss/test-utils';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return (
+    <I18nProvider>
+      <ThemeProvider>{children}</ThemeProvider>
+    </I18nProvider>
+  );
 };
 
 setOptions({ wrapper: Providers });
