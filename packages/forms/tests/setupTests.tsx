4cf1648c4388a231770bcc028f65a878560b8b68
Eron Alves <eron.alves@rocketmail.com>
2023-06-01 14:07:20 -0300

    refactor: remove provider

--- packages/forms/tests/setupTests.tsx
+++ packages/forms/tests/setupTests.tsx
@@ -1,14 +1,9 @@
 import * as React from 'react';
-import { I18nProvider } from '@ttoss/react-i18n';
 import { ThemeProvider } from '@ttoss/ui';
 import { setOptions } from '@ttoss/test-utils';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <I18nProvider>
-      <ThemeProvider>{children}</ThemeProvider>
-    </I18nProvider>
-  );
+  return <ThemeProvider>{children}</ThemeProvider>;
 };
 
 setOptions({ wrapper: Providers });
