bbc22413a995d2e54c6de4b3aacc1b85230016fc
Eron Alves <eron.alves@rocketmail.com>
2023-06-02 16:10:36 -0300

    fix: error not apearing in nested fields

--- packages/forms/src/ErrorMessage.tsx
+++ packages/forms/src/ErrorMessage.tsx
@@ -1,4 +1,10 @@
-import { FieldName, FieldValues, useFormContext } from 'react-hook-form';
+import {
+  FieldError,
+  FieldErrors,
+  FieldName,
+  FieldValues,
+  useFormContext,
+} from 'react-hook-form';
 import { FormattedMessage, MessageDescriptor } from '@ttoss/react-i18n';
 import { HelpText } from '@ttoss/ui';
 
@@ -25,16 +31,24 @@ export const ErrorMessage = <TFieldValues extends FieldValues = FieldValues>({
     formState: { errors },
   } = useFormContext<TFieldValues>();
 
-  const error = errors[name];
+  const error = name
+    .split('.')
+    .reduce<FieldErrors<FieldValues> | FieldError | string | undefined>(
+      (rest, n) => {
+        if (!rest) return undefined;
+        return rest[n as keyof typeof rest];
+      },
+      errors
+    );
 
-  if (!error) return null;
+  if (!error || typeof error === 'string') return null;
 
   const { message } = error;
 
   return (
     error && (
       <HelpText negative disabled={disabled}>
-        {isMessageDescriptor(message) ? (
+        {isMessageDescriptor(message) && !(typeof message === 'string') ? (
           <FormattedMessage {...message} />
         ) : (
           (message as string)
