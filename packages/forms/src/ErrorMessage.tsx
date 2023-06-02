c3959c9e04e864f63cb50a4c61b8e7a084cdeb70
EronAlves1996 <eron.alves@rocketmail.com>
2023-06-02 17:46:28 -0300

    Fix/nested forms (#376)
    * test: add test to catch error
    
    * fix: error not apearing in nested fields
    
    * chore: install error message component
    
    * refactor: prefer hookFormMessage componetn
    
    * refactor: prefer IIFE over ternary operator

--- packages/forms/src/ErrorMessage.tsx
+++ packages/forms/src/ErrorMessage.tsx
@@ -1,6 +1,13 @@
-import { FieldName, FieldValues, useFormContext } from 'react-hook-form';
+import {
+  FieldError,
+  FieldErrors,
+  FieldName,
+  FieldValues,
+  useFormContext,
+} from 'react-hook-form';
 import { FormattedMessage, MessageDescriptor } from '@ttoss/react-i18n';
 import { HelpText } from '@ttoss/ui';
+import { ErrorMessage as HookformErrorMessage } from '@hookform/error-message';
 
 type ModifiedDescriptor = MessageDescriptor & { values?: any };
 
@@ -25,21 +32,24 @@ export const ErrorMessage = <TFieldValues extends FieldValues = FieldValues>({
     formState: { errors },
   } = useFormContext<TFieldValues>();
 
-  const error = errors[name];
-
-  if (!error) return null;
-
-  const { message } = error;
-
   return (
-    error && (
-      <HelpText negative disabled={disabled}>
-        {isMessageDescriptor(message) ? (
-          <FormattedMessage {...message} />
-        ) : (
-          (message as string)
-        )}
-      </HelpText>
-    )
+    <HookformErrorMessage
+      name={name as any}
+      errors={errors}
+      render={({ message }: { message: FieldError | string }) => {
+        return (
+          <HelpText negative disabled={disabled}>
+            {(() => {
+              if (typeof message === 'string') return message;
+
+              if (isMessageDescriptor(message))
+                return <FormattedMessage {...message} />;
+
+              return JSON.stringify(message);
+            })()}
+          </HelpText>
+        );
+      }}
+    />
   );
 };
