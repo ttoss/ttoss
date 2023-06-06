ad7dba82f795af302cea520720d3fcd3064eb650
Pedro Arantes <arantespp@gmail.com>
2023-06-06 18:51:24 -0300

    fix: remove file extension

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -2,12 +2,7 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   extends: ['./config.js', 'next/core-web-vitals'],
-  overrides: [
-    {
-      files: ['**/*.tsx'],
-      rules: {
-        'import/no-default-export': 'off',
-      },
-    },
-  ],
+  rules: {
+    'import/no-default-export': 'off',
+  },
 };
