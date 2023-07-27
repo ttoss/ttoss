95428cb8a72c502cc0e40652a2ef08b72d1c3377
EronAlves1996 <eron.alves@rocketmail.com>
2023-07-27 15:48:13 -0300

    test: test notification resolution algorithm

--- packages/react-notifications/tests/unit/NotificationsBox.test.tsx
+++ packages/react-notifications/tests/unit/NotificationsBox.test.tsx
@@ -4,10 +4,14 @@ import { render, screen, userEvent } from '@ttoss/test-utils';
 
 const NOTIFICATION_MESSAGE = 'notification message test';
 
-test('should render single notification message', async () => {
+describe('Notifications Box Test', () => {
   const user = userEvent.setup({ delay: null });
 
-  const Component = () => {
+  const Component = ({
+    notifications,
+  }: {
+    notifications: NotifyParams | NotifyParams[];
+  }) => {
     const { setNotifications } = useNotifications();
 
     return (
@@ -15,7 +19,7 @@ test('should render single notification message', async () => {
         <NotificationsBox />
         <button
           onClick={() => {
-            setNotifications({ message: NOTIFICATION_MESSAGE, type: 'error' });
+            setNotifications(notifications);
           }}
           type="button"
         >
@@ -25,58 +29,92 @@ test('should render single notification message', async () => {
     );
   };
 
-  render(<Component />, { wrapper: NotificationsProvider });
-
-  expect(screen.getByRole('button')).toBeInTheDocument();
-
-  await user.click(screen.getByRole('button'));
-
-  expect(screen.getByText(NOTIFICATION_MESSAGE)).toBeInTheDocument();
-});
-
-test('should render a list of notification messages', async () => {
-  const user = userEvent.setup({ delay: null });
-
-  const numberOfMessages = 5;
-
-  const Component = () => {
-    const { setNotifications } = useNotifications();
-
-    return (
-      <div>
-        <NotificationsBox />
-        <button
-          onClick={() => {
-            const list: NotifyParams[] = Array.from({
-              length: numberOfMessages,
-            }).map((_, idx) => {
-              return {
-                message: `${NOTIFICATION_MESSAGE}-${idx + 1}`,
-                type: 'error',
-              };
-            });
-
-            setNotifications(list);
-          }}
-          type="button"
-        >
-          click
-        </button>
-      </div>
+  test('should render single notification message', async () => {
+    render(
+      <Component
+        notifications={{ message: NOTIFICATION_MESSAGE, type: 'error' }}
+      />,
+      { wrapper: NotificationsProvider }
     );
-  };
 
-  render(<Component />, { wrapper: NotificationsProvider });
+    expect(screen.getByRole('button')).toBeInTheDocument();
 
-  expect(screen.getByRole('button')).toBeInTheDocument();
+    await user.click(screen.getByRole('button'));
 
-  await user.click(screen.getByRole('button'));
+    expect(screen.getByText(NOTIFICATION_MESSAGE)).toBeInTheDocument();
+  });
 
-  Array.from({
-    length: numberOfMessages,
-  }).forEach((_, idx) => {
-    expect(
-      screen.getByText(`${NOTIFICATION_MESSAGE}-${idx + 1}`)
-    ).toBeInTheDocument();
+  test('should render a list of notification messages', async () => {
+    const numberOfMessages = 5;
+
+    render(
+      <Component
+        notifications={Array.from({
+          length: numberOfMessages,
+        }).map<NotifyParams>((_, idx) => {
+          return {
+            message: `${NOTIFICATION_MESSAGE}-${idx + 1}`,
+            type: 'error',
+          };
+        })}
+      />,
+      {
+        wrapper: NotificationsProvider,
+      }
+    );
+
+    expect(screen.getByRole('button')).toBeInTheDocument();
+
+    await user.click(screen.getByRole('button'));
+
+    Array.from({
+      length: numberOfMessages,
+    }).forEach((_, idx) => {
+      expect(
+        screen.getByText(`${NOTIFICATION_MESSAGE}-${idx + 1}`)
+      ).toBeInTheDocument();
+    });
+  });
+
+  test('Should resolve repeated notifications correctly', async () => {
+    const repeatedNotifications: Array<NotifyParams> = [
+      { message: 'single message', type: 'error' },
+      { message: 'single message', type: 'error' },
+    ];
+
+    render(<Component notifications={repeatedNotifications} />, {
+      wrapper: NotificationsProvider,
+    });
+
+    expect(screen.getByRole('button')).toBeInTheDocument();
+
+    await user.click(screen.getByRole('button'));
+
+    const elements = screen.getAllByText('single message');
+
+    expect(elements.length).toBe(2);
+
+    await user.click(elements[0]);
+
+    expect(screen.getByText('single message')).toBeInTheDocument();
+  });
+
+  test('Should resolve notifications with the same key', async () => {
+    const keyedNotifications: Array<NotifyParams> = [
+      { message: 'single message', type: 'error', notificationKey: 'message' },
+      { message: 'single message', type: 'error', notificationKey: 'message' },
+    ];
+
+    render(<Component notifications={keyedNotifications} />, {
+      wrapper: NotificationsProvider,
+    });
+
+    expect(screen.getByRole('button')).toBeInTheDocument();
+
+    await user.click(screen.getByRole('button'));
+
+    const elements = screen.getAllByText('single message');
+
+    expect(elements.length).toBe(1);
   });
 });
