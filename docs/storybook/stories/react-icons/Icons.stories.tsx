1c4601bac33a593bb4c68c75d8aaf01ef9daec28
Pedro Arantes <arantespp@gmail.com>
2023-08-15 17:45:00 -0300

    docs: add type to icons docs

--- docs/storybook/stories/react-icons/Icons.stories.tsx
+++ docs/storybook/stories/react-icons/Icons.stories.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Flex, Grid, Label, Select, Slider, Text, useTheme } from '@ttoss/ui';
-import { Icon, addIcon } from '@ttoss/react-icons';
+import { Icon, type IconType, addIcon } from '@ttoss/react-icons';
 import { Meta, Story } from '@storybook/react';
 
 export default {
@@ -175,13 +175,15 @@ export const CoreIcons = () => {
   );
 };
 
-addIcon('custom-search', {
+const customSearchIcon: IconType = {
   body: `<svg xmlns="http://www.w3.org/2000/svg" width="48px" height="48px" viewBox="0 0 24 24" fill="none">
   <path d="M15.8053 15.8013L21 21M10.5 7.5V13.5M7.5 10.5H13.5M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
   </svg>`,
   width: 48,
   height: 48,
-});
+};
+
+addIcon('custom-search', customSearchIcon);
 
 export const CustomIcon = () => {
   return (
--- packages/react-icons/README.md
+++ packages/react-icons/README.md
@@ -46,15 +46,17 @@ export const IconExample = () => {
 You can add your own icons to the library using `addIcon` function. This function receives the name of the icon and the icon object.
 
 ```tsx
-import { Icon, addIcon } from '@ttoss/react-icons';
+import { Icon, addIcon, type IconType } from '@ttoss/react-icons';
 
-addIcon('custom-search', {
+const customSearchIcon: IconType = {
   body: `<svg xmlns="http://www.w3.org/2000/svg" width="48px" height="48px" viewBox="0 0 24 24" fill="none">
   <path d="M15.8053 15.8013L21 21M10.5 7.5V13.5M7.5 10.5H13.5M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
   </svg>`,
   width: 48,
   height: 48,
-});
+};
+
+addIcon('custom-search', customSearchIcon);
 
 export const CustomIcon = () => {
   return (
