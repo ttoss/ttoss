5d31020e2b8826368bcbf1c355633cc286fc4c34
RayzaOliveira <rayza.ocr@gmail.com>
2023-08-16 16:38:47 -0300

    fix: fixed tests to react-auth lib

--- packages/react-auth/jest.config.ts
+++ packages/react-auth/jest.config.ts
@@ -3,6 +3,12 @@ import { jestConfig } from '@ttoss/config';
 const config = jestConfig({
   setupFilesAfterEnv: ['./tests/setupTests.tsx'],
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    'react-markdown': [
+      '<rootDir>/../../node_modules/@ttoss/components/node_modules/react-markdown/react-markdown.min.js',
+    ],
+  },
 });
 
+// eslint-disable-next-line import/no-default-export
 export default config;
