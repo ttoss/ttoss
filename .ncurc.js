7270d0f4585b0660c9381740bdba042409a84b45
Pedro Arantes <arantespp@gmail.com>
2023-11-22 22:28:59 -0300

    chore: update test-utils package

--- .ncurc.js
+++ .ncurc.js
@@ -1,11 +1,16 @@
 module.exports = {
   reject: [
     /**
+     * @testing-library/jest-dom v6 breaks TypeScript custom matchers.
+     * Example https://github.com/testing-library/jest-dom/issues/546
+     */
+    '@testing-library/jest-dom',
+    /**
      * change-case v5 is ESM and carlin is CJS.
      */
     'change-case',
     /**
-     * On 2023-11-22, updating to the latest version of `typescript` will
+     * On 2023-11-22, updating to the 5.3 version of `typescript` will
      * cause some errors:
      * - extends `@ttoss/config/tsconfig.json` will cause an error.
      * - tests with userEvent.setup will fail.
