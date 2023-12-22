d692b6c741fef924d2ca09c1b1e0d1a757f4eea4
GiovaneGuimaraes <133304083+GiovaneGuimaraes@users.noreply.github.com>
2023-12-22 12:38:28 -0300

    feat: migrate helix to yoga (#457)
    * feat: migrate helix to yoga
    
    * fix: export modules with same name
    
    * feat: create graphql schema
    
    * feat: create graphql-yoga server with tests
    
    * fix: tests
    
    * fix: packages and pnpm i
    
    * fix: build terezinha farm app problem

---
+++ packages/graphql-api-server/tests/schemaComposer.ts
@@ -0,0 +1,122 @@
+import {
+  ResolverResolveParams,
+  composeWithConnection,
+  schemaComposer,
+} from '@ttoss/graphql-api';
+
+const AuthorTC = schemaComposer.createObjectTC({
+  name: 'Author',
+  fields: {
+    id: 'Int!',
+    name: 'String!',
+  },
+});
+
+AuthorTC.addResolver({
+  name: 'findMany',
+  type: AuthorTC,
+  resolve: async ({
+    args,
+    rawQuery,
+  }: ResolverResolveParams<unknown, unknown, { limit: number }>) => {
+    if (rawQuery.id?.$lt) {
+      return [...new Array(args.limit)]
+        .map((_, index) => {
+          const id = Number(rawQuery.id.$lt || 0) - index - 1;
+          return {
+            id,
+            name: `Author ${id}`,
+          };
+        })
+        .filter((author) => {
+          return author.id > 0;
+        });
+    }
+
+    return [...new Array(args.limit)]
+      .map((_, index) => {
+        const id = index + 1 + Number(rawQuery.id?.$gt || 0);
+        return {
+          id,
+          name: `Author ${id}`,
+        };
+      })
+      .filter((author) => {
+        return author.id < 100;
+      });
+  },
+});
+
+AuthorTC.addResolver({
+  name: 'count',
+  type: 'Int!',
+  resolve: async () => {
+    return 99;
+  },
+});
+
+composeWithConnection(AuthorTC, {
+  findManyResolver: AuthorTC.getResolver('findMany'),
+  countResolver: AuthorTC.getResolver('count'),
+  defaultLimit: 10,
+  sort: {
+    ID_ASC: {
+      value: {},
+      cursorFields: ['id'],
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars, max-params
+      beforeCursorQuery: (rawQuery, cursorData, resolveParams) => {
+        if (!rawQuery.id) {
+          rawQuery.id = {};
+        }
+        rawQuery.id.$lt = cursorData.id;
+      },
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars, max-params
+      afterCursorQuery: (rawQuery, cursorData, resolveParams) => {
+        if (!rawQuery.id) {
+          rawQuery.id = {};
+        }
+        rawQuery.id.$gt = cursorData.id;
+      },
+    },
+  },
+});
+
+schemaComposer.Mutation.addFields({
+  createAuthor: {
+    type: AuthorTC,
+    args: {
+      id: 'ID!',
+      name: 'String!',
+    },
+  },
+});
+
+AuthorTC.addResolver({
+  name: 'createAuthor',
+  type: AuthorTC,
+  args: {
+    id: 'ID!', // Defina os tipos dos argumentos conforme necessário
+    name: 'String!',
+  },
+  resolve: async ({
+    args,
+  }: ResolverResolveParams<unknown, unknown, { id: string; name: string }>) => {
+    const { id, name } = args;
+    const newAuthor = {
+      id,
+      name,
+    };
+
+    return newAuthor;
+  },
+});
+
+schemaComposer.Query.addFields({
+  author: AuthorTC.getResolver('connection'),
+});
+
+schemaComposer.Mutation.addFields({
+  createAuthor: AuthorTC.getResolver('createAuthor'),
+});
+
+export { schemaComposer };
