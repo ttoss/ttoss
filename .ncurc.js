8e2eb67689f8b7107a42cbe71ca64dae04e64192
Pedro Arantes <arantespp@gmail.com>
2024-01-29 14:00:31 -0300

    fix: add next to ncurc

--- .ncurc.js
+++ .ncurc.js
@@ -10,6 +10,12 @@ module.exports = {
      */
     'change-case',
     /**
+     * On 2024-01-29, updating to the 14.1.0 version of `next` will
+     * cause dev and build to fail.
+     * https://github.com/vercel/next.js/issues/61116
+     */
+    'next',
+    /**
      * On 2023-11-22, updating to the 5.3 version of `typescript` will
      * cause some errors:
      * - extends `@ttoss/config/tsconfig.json` will cause an error.
