b369d9f4b2cff3a206d95cf7c248a8b7e835f595
Rayza Oliveira <52584988+RayzaOliveira@users.noreply.github.com>
2024-02-22 16:31:11 -0300

    Feat/badge chip (#491)
    * feat: create chip variant to Badge component
    
    * docs: create story to Badge component with chip prop
    
    * test: create tests to Badge component
    
    * refactor: replace Text importation

--- packages/ui/src/components/Badge.tsx
+++ packages/ui/src/components/Badge.tsx
@@ -1,11 +1,21 @@
 import { type BadgeProps as BadgePropsUi, Badge as BadgeUi } from 'theme-ui';
 import { Icon, IconType } from '@ttoss/react-icons';
+import { Text } from '../components/Text';
 
 export type BadgeProps = BadgePropsUi & {
   icon?: IconType;
+  chip?: boolean;
+  onDelete?: () => void;
 };
 
-export const Badge = ({ icon, children, sx, ...props }: BadgeProps) => {
+export const Badge = ({
+  icon,
+  children,
+  sx,
+  chip,
+  onDelete,
+  ...props
+}: BadgeProps) => {
   return (
     <BadgeUi
       sx={{
@@ -24,7 +34,21 @@ export const Badge = ({ icon, children, sx, ...props }: BadgeProps) => {
       {...props}
     >
       {icon && <Icon inline icon={icon} />}
+
       {children}
+
+      {chip && (
+        <Text
+          sx={{
+            cursor: 'pointer',
+            lineHeight: 0,
+            color: 'currentcolor',
+            alignSelf: 'center',
+          }}
+        >
+          <Icon onClick={onDelete} inline icon="close" />
+        </Text>
+      )}
     </BadgeUi>
   );
 };
