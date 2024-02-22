1afdb8d2e348a4983149b298b4050bdad99018a0
RayzaOliveira <rayza.ocr@gmail.com>
2024-02-22 11:21:09 -0300

    feat: create chip variant to Badge component

--- packages/ui/src/components/Badge.tsx
+++ packages/ui/src/components/Badge.tsx
@@ -1,11 +1,24 @@
-import { type BadgeProps as BadgePropsUi, Badge as BadgeUi } from 'theme-ui';
+import {
+  type BadgeProps as BadgePropsUi,
+  Badge as BadgeUi,
+  Text,
+} from 'theme-ui';
 import { Icon, IconType } from '@ttoss/react-icons';
 
 export type BadgeProps = BadgePropsUi & {
   icon?: IconType;
+  chip?: boolean;
+  onDelete?: () => void;
 };
 
-export const Badge = ({ icon, children, sx, ...props }: BadgeProps) => {
+export const Badge = ({
+  icon,
+  children,
+  sx,
+  chip,
+  onDelete,
+  ...props
+}: BadgeProps) => {
   return (
     <BadgeUi
       sx={{
@@ -24,7 +37,21 @@ export const Badge = ({ icon, children, sx, ...props }: BadgeProps) => {
       {...props}
     >
       {icon && <Icon inline icon={icon} />}
+
       {children}
+
+      {chip && (
+        <Text
+          sx={{
+            cursor: 'pointer',
+            lineHeight: 0,
+            color: 'currentcolor',
+            alignSelf: 'center',
+          }}
+        >
+          <Icon onClick={onDelete} inline icon="close" />
+        </Text>
+      )}
     </BadgeUi>
   );
 };
