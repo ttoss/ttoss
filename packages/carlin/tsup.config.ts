315d6f6a9ea4c8dd44b33fa6f4944aa38a97f329
Pedro Arantes <arantespp@gmail.com>
2024-02-24 12:11:54 -0300

    fix: carlin build config

--- packages/carlin/tsup.config.ts
+++ packages/carlin/tsup.config.ts
@@ -3,8 +3,5 @@ import { defineConfig } from 'tsup';
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   entry: ['src/index.ts'],
-  splitting: false,
-  sourcemap: false,
-  clean: false,
-  format: ['cjs'],
+  format: 'cjs',
 });
