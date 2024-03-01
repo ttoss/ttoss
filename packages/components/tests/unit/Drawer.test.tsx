126c806b278195c9dcd38aaea880bf2d0ebcc578
RayzaOliveira <rayza.ocr@gmail.com>
2024-03-01 11:24:20 -0300

    test: add tests to Drawer component

---
+++ packages/components/tests/unit/Drawer.test.tsx
@@ -0,0 +1,38 @@
+import { Drawer } from '../../src';
+import { render, screen } from '@ttoss/test-utils';
+
+describe('Drawer', () => {
+  test('should renders children content', () => {
+    const testMessage = 'Test content';
+    render(
+      <Drawer direction="right" open={true}>
+        <div>{testMessage}</div>
+      </Drawer>
+    );
+
+    expect(screen.getByText(testMessage)).toBeInTheDocument();
+  });
+
+  test('should applies custom styles from sx prop', () => {
+    const customStyle = { backgroundColor: 'red' };
+    render(
+      <Drawer open={true} direction="right" sx={customStyle}>
+        <div>Test content</div>
+      </Drawer>
+    );
+
+    const boxComponent = screen.getByRole('navigation').firstChild;
+    expect(boxComponent).toHaveStyle(customStyle);
+  });
+
+  test('should propagates props to DrawerUi component', () => {
+    const testMessage = 'Test content';
+    render(
+      <Drawer direction="right" open={true}>
+        <div>{testMessage}</div>
+      </Drawer>
+    );
+
+    expect(screen.getByText(testMessage)).toBeVisible();
+  });
+});
