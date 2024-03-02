4fb29b3fd9f7992d26978ab638b29d15f8b4f9af
RayzaOliveira <rayza.ocr@gmail.com>
2024-03-01 23:43:59 -0300

    feat: update Menu component to reuse Drawer component

--- packages/components/src/components/Menu.tsx
+++ packages/components/src/components/Menu.tsx
@@ -1,75 +1,107 @@
 import * as React from 'react';
-import { Box, BoxProps, Flex, Image, Text } from '@ttoss/ui';
-import { Icon } from '@ttoss/react-icons';
+import {
+  Box,
+  BoxProps,
+  Flex,
+  Image,
+  Text,
+  useResponsiveValue,
+} from '@ttoss/ui';
+import { Drawer } from '..';
+import { Icon, IconifyIcon } from '@ttoss/react-icons';
 
 export type MenuProps = {
   onClose: () => void;
+  onOpen: () => void;
   isOpen: boolean;
   children: React.ReactNode;
   srcLogo?: string;
   sx?: BoxProps['sx'];
+  /**
+   * Default direction: `left`
+   */
+  direction?: 'left' | 'right';
+  /**
+   * Default size: `100%`
+   */
+  sizeOnMobile?: string | number;
+  /**
+   * Default size: `300px`
+   */
+  sizeOnDesktop?: string | number;
+  /**
+   * Default icon: `menu-open`
+   */
+  menuIcon?: IconifyIcon | string;
 };
 
-export const Menu = ({ children, srcLogo, onClose, isOpen, sx }: MenuProps) => {
+export const Menu = ({
+  children,
+  srcLogo,
+  onClose,
+  onOpen,
+  isOpen,
+  sx,
+  direction = 'left',
+  sizeOnDesktop = '300px',
+  sizeOnMobile = '100%',
+  menuIcon = 'menu-open',
+}: MenuProps) => {
+  const responsiveSize = useResponsiveValue([sizeOnMobile, sizeOnDesktop]);
+
   return (
-    <Box
-      sx={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        height: '100vh',
-        width: '100vw',
-        zIndex: 50,
-        transform: isOpen ? 'translateX(0)' : 'translateX(100%)',
-        transition: 'transform 500ms',
-        ...sx,
-      }}
-      aria-hidden={!isOpen}
-    >
-      <Box
-        sx={{
-          position: 'fixed',
-          height: '100%',
-          backgroundColor: 'white',
-          width: '100%',
-          maxWidth: 'md',
-          right: 0,
-          boxShadow: 'lg',
-          paddingX: 'xl',
-          paddingTop: 'lg',
-          paddingBottom: '2xl',
-        }}
-      >
-        <Flex sx={{ justifyContent: 'space-between' }}>
-          <Image
-            src={srcLogo}
-            sx={{
-              maxWidth: '200px',
-              height: '44px',
-              objectFit: 'cover',
-            }}
-          />
+    <>
+      <Text sx={{ cursor: 'pointer' }} onClick={onOpen}>
+        <Icon icon={menuIcon} />
+      </Text>
 
-          <Text
-            sx={{
-              marginLeft: 'auto',
-              fontSize: '2xl',
-              alignSelf: 'center',
-              flexShrink: 0,
-              cursor: 'pointer',
-              lineHeight: 0,
-            }}
-            role="button"
-            onClick={onClose}
-          >
-            <Icon icon="close" />
-          </Text>
-        </Flex>
+      <Drawer size={responsiveSize} direction={direction} open={isOpen}>
+        <Box
+          sx={{
+            position: 'fixed',
+            height: '100%',
+            backgroundColor: 'background',
+            width: '100%',
+            maxWidth: 'md',
+            right: 0,
+            boxShadow: 'lg',
+            paddingX: 'xl',
+            paddingTop: 'lg',
+            paddingBottom: '2xl',
+            ...sx,
+          }}
+        >
+          <Flex sx={{ justifyContent: 'space-between' }}>
+            <Image
+              src={srcLogo}
+              sx={{
+                maxWidth: '200px',
+                height: '44px',
+                objectFit: 'cover',
+              }}
+            />
 
-        <Box sx={{ paddingTop: '3xl' }} as="nav">
-          {children}
+            <Text
+              sx={{
+                marginLeft: 'auto',
+                fontSize: '2xl',
+                alignSelf: 'center',
+                flexShrink: 0,
+                cursor: 'pointer',
+                lineHeight: 0,
+              }}
+              role="button"
+              onClick={onClose}
+            >
+              <Icon icon="close" />
+            </Text>
+          </Flex>
+
+          <Box sx={{ paddingTop: '3xl' }} as="nav">
+            {children}
+          </Box>
         </Box>
-      </Box>
-    </Box>
+      </Drawer>
+    </>
   );
 };
