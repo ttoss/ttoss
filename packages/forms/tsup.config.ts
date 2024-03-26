56b201b84c8155a157564ac258cabef25c1ab620
GiovaneGuimaraes <gioguima24@gmail.com>
2024-03-26 15:19:19 -0300

    fix: tsup config file

--- packages/forms/tsup.config.ts
+++ packages/forms/tsup.config.ts
@@ -1,5 +1,9 @@
 import { tsupConfig } from '@ttoss/config';
 
 export const tsup = tsupConfig({
-  entryPoints: ['src/index.ts', 'src/MultistepForm/index.ts'],
+  entryPoints: [
+    'src/index.ts',
+    'src/MultistepForm/index.ts',
+    'src/Brazil/index.ts',
+  ],
 });
