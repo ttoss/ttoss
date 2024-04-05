254db1acd4461df6170230f2cdbf2666f91f6b66
Mateus Henrique <mateushbsb@gmail.com>
2024-04-05 08:43:37 -0300

    feat: create component list

---
+++ docs/storybook/stories/components/List.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, StoryFn } from '@storybook/react';
+
+import { List, ListItem } from '@ttoss/ui';
+
+export default {
+  title: 'Components/List',
+  component: List,
+} as Meta;
+
+const Template: StoryFn = (args) => {
+  return (
+    <List {...args}>
+      <ListItem>Item 1</ListItem>
+      <ListItem>Item 2</ListItem>
+      <ListItem>Item 3</ListItem>
+    </List>
+  );
+};
+
+export const componentsListDefault = Template.bind({});
