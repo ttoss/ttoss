81c078a88d6c50311e7705061ee54a25d4a16f75
Pedro Arantes <arantespp@gmail.com>
2024-04-16 16:39:13 -0300

    chore: downgrade eslint to v8

--- .ncurc.js
+++ .ncurc.js
@@ -10,6 +10,10 @@ module.exports = {
      */
     'change-case',
     /**
+     * Waiting for https://github.com/ttoss/ttoss/issues/526
+     */
+    'eslint',
+    /**
      * On 2024-01-29, updating to the 14.1.0 version of `next` will
      * cause dev and build to fail.
      * https://github.com/vercel/next.js/issues/61116
