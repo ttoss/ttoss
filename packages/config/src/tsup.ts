513ae116b372c2da70335e9b9928c4e55d0bcb0a
Pedro Arantes <arantespp@gmail.com>
2024-05-04 12:40:20 -0300

    fix: change ES2021 to es2021

--- packages/config/src/tsup.ts
+++ packages/config/src/tsup.ts
@@ -1,3 +1,4 @@
+import * as typescriptConfig from './typescriptConfig';
 import { Plugin, PluginBuild } from 'esbuild';
 import { configCreator } from './configCreator';
 import { transformAsync } from '@babel/core';
@@ -60,11 +61,13 @@ export const defaultConfig: Options = {
     js: `/** Powered by @ttoss/config. https://ttoss.dev/docs/modules/packages/config/ */`,
   },
   esbuildPlugins: [formatjsPlugin],
+  target: typescriptConfig.target,
 };
 
-export const tsupConfig = configCreator</**
+/**
  * any on configCreator to avoid error "The inferred type of 'tsup' cannot
  * be named without a reference to '.../node_modules/tsup'. This is likely not
  * portable. A type annotation is necessary."
  */
-any>(defaultConfig);
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const tsupConfig = configCreator<any>(defaultConfig);
--- packages/config/src/typescriptConfig.ts
+++ packages/config/src/typescriptConfig.ts
@@ -1,5 +1,5 @@
 /**
- * `target` to `ES2021` because Node.js 20 supports ES2021 features.
+ * `target` to `es2021` because Node.js 20 supports ES2021 features.
  * https://node.green/#ES2021
  */
-export const target = 'ES2021';
+export const target = 'es2021';
