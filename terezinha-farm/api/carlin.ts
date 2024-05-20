16251e4b577c9bb60b51c241f7d64e9073d73c87
Pedro Arantes <arantespp@gmail.com>
2024-05-20 19:18:01 -0300

    fix: remove test

--- terezinha-farm/api/carlin.ts
+++ terezinha-farm/api/carlin.ts
@@ -1,6 +1,3 @@
-// eslint-disable-next-line turbo/no-undeclared-env-vars, no-console
-console.log(process.env.TEST_VAR);
-
 // eslint-disable-next-line import/no-default-export
 export default async () => {
   return {
