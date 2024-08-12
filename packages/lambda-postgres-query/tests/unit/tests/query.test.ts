2738267f4d6785a9186535ea284ac8bd0fdebd92
Pedro Arantes <arantespp@gmail.com>
2024-08-12 17:35:25 -0300

    test: add more tests

--- packages/lambda-postgres-query/tests/unit/tests/query.test.ts
+++ packages/lambda-postgres-query/tests/unit/tests/query.test.ts
@@ -8,6 +8,29 @@ beforeEach(() => {
   lambdaMock.reset();
 });
 
+test('should accept string or object as parameters', async () => {
+  const text = 'SELECT * FROM table';
+
+  lambdaMock
+    .on(InvokeCommand, {
+      Payload: JSON.stringify({ readOnly: true, text }),
+    })
+    .resolves({
+      Payload: new TextEncoder().encode(
+        JSON.stringify({
+          rows: [{ id: 1 }],
+        })
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      ) as any,
+    });
+
+  const result1 = await query(text);
+  const result2 = await query({ text });
+
+  expect(result1.rows).toEqual([{ id: 1 }]);
+  expect(result2.rows).toEqual([{ id: 1 }]);
+});
+
 test('should add camelCaseKeys to the rows', async () => {
   lambdaMock.on(InvokeCommand).resolves({
     Payload: new TextEncoder().encode(
