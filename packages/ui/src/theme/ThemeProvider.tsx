db5c67011af4020dd814868d7be0452588f51972
Pedro Arantes <arantespp@gmail.com>
2024-08-29 12:21:30 -0300

    fix: remove unused provider

--- packages/ui/src/theme/ThemeProvider.tsx
+++ packages/ui/src/theme/ThemeProvider.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { BruttalFonts, BruttalTheme } from '@ttoss/theme/Bruttal';
-import { ChakraProvider } from '@chakra-ui/react';
 import { Global, css } from '@emotion/react';
 import { Theme, ThemeUIProvider } from 'theme-ui';
 
@@ -21,18 +20,16 @@ export const ThemeProvider = ({
   return (
     <>
       <ThemeUIProvider theme={theme}>
-        <ChakraProvider theme={theme}>
-          <Global
-            styles={css`
-              ${fonts
-                .map((url) => {
-                  return `@import url('${url}');`;
-                })
-                .join('\n')}
-            `}
-          />
-          {children}
-        </ChakraProvider>
+        <Global
+          styles={css`
+            ${fonts
+              .map((url) => {
+                return `@import url('${url}');`;
+              })
+              .join('\n')}
+          `}
+        />
+        {children}
       </ThemeUIProvider>
     </>
   );
