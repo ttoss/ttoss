7dacff8c96cf7453366757da02645c1e56601205
Pedro Arantes <arantespp@gmail.com>
2024-10-07 14:00:05 -0300

    docs: improve postgresdb docs (#605)

--- packages/postgresdb/tsup.config.ts
+++ packages/postgresdb/tsup.config.ts
@@ -1,6 +1,7 @@
+import { defineConfig } from 'tsup';
 import { tsupConfig } from '@ttoss/config';
 
-export const tsup = {
+export const tsup = defineConfig({
   ...tsupConfig(),
   format: ['esm'],
-};
+});
