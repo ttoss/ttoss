ab4f75cd14eb5bd9cd593e1c1abbba8aa4440165
GiovaneGuimaraes <133304083+GiovaneGuimaraes@users.noreply.github.com>
2024-10-14 15:53:01 -0300

    feat: add verify if sign in result is reset password (#588)
    * feat: add verify if sign in result is reset password
    
    * feat: add test to rest_password verify
    
    * fix: remove unecessary export

---
+++ packages/react-auth/tests/unit/tests/AuthNotification.test.tsx
@@ -0,0 +1,73 @@
+import { AuthSignIn } from 'src/AuthSignIn';
+import { render, screen, userEvent } from '@ttoss/test-utils';
+import { signIn } from 'aws-amplify/auth';
+import { waitFor } from '@ttoss/test-utils';
+
+/**
+ * mock signIn aws-amplify to return value RESET_PASSWORD
+ */
+jest.mock('aws-amplify/auth', () => {
+  return {
+    signIn: jest.fn().mockResolvedValue({
+      nextStep: {
+        signInStep: 'RESET_PASSWORD',
+      },
+      isSignedIn: false,
+    }),
+  };
+});
+
+const onSignUp = jest.fn();
+const onForgotPassword = jest.fn();
+
+const userForm = {
+  username: 'user@example.com',
+  password: 'password',
+};
+
+test('Should show error notification if the nextStep.signInStep is RESET_PASSWORD', async () => {
+  const user = userEvent.setup();
+
+  const setNotifications = jest.fn();
+
+  /**
+   * mock onSignIn
+   */
+  const onSignIn = jest.fn(async ({ email, password }) => {
+    const result = await signIn({ username: email, password });
+    if (result.nextStep.signInStep === 'RESET_PASSWORD') {
+      setNotifications({
+        type: 'error',
+        message: `For your security, we have updated our system and you need to reset your password in 'forgot your password?' to proceed`,
+      });
+    }
+  });
+
+  render(
+    <AuthSignIn
+      onSignIn={onSignIn}
+      onSignUp={onSignUp}
+      onForgotPassword={onForgotPassword}
+    />
+  );
+
+  const emailInput = screen.getByLabelText('Email');
+  const password = screen.getByLabelText('Password');
+  const [buttonSubmit] = screen.getAllByRole('button');
+
+  await user.type(emailInput, userForm.username);
+  await user.type(password, userForm.password);
+  await user.click(buttonSubmit);
+
+  await waitFor(() => {
+    expect(onSignIn).toHaveBeenCalledWith({
+      email: userForm.username,
+      password: userForm.password,
+    });
+  });
+
+  expect(setNotifications).toHaveBeenCalledWith({
+    type: 'error',
+    message: `For your security, we have updated our system and you need to reset your password in 'forgot your password?' to proceed`,
+  });
+});
