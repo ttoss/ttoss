819604cd1f013fd3c339f3e1828a67fa4d59b5d1
Pedro Arantes <arantespp@gmail.com>
2024-12-03 12:13:07 -0300

    fix: remove import star from sequelize-typescript

---
+++ packages/postgresdb/src/sequelize-typescript.ts
@@ -0,0 +1,21 @@
+/**
+ * Exports the necessary classes from the sequelize-typescript package.
+ */
+export {
+  BelongsTo,
+  BelongsToMany,
+  Column,
+  CreatedAt,
+  DataType,
+  DeletedAt,
+  ForeignKey,
+  HasMany,
+  HasOne,
+  Index,
+  Model,
+  Sequelize,
+  Table,
+  UpdatedAt,
+} from 'sequelize-typescript';
+
+export type { ModelCtor, SequelizeOptions } from 'sequelize-typescript';
