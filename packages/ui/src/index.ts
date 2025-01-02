17ea065d5e1a6766f849ea1ca1875b731b34b431
Pedro Arantes <arantespp@gmail.com>
2025-01-02 11:49:23 -0300

    fix: eslint import rule

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,58 +1,55 @@
 export {
-  BaseStyles,
-  type Theme,
-  Global,
-  type SxProp,
-  type ThemeUIStyleObject,
-} from 'theme-ui';
-export { useResponsiveValue, useBreakpointIndex } from '@theme-ui/match-media';
-export { keyframes, type Keyframes } from '@emotion/react';
-
-export { ThemeProvider, type ThemeProviderProps } from './theme/ThemeProvider';
-
-export { useTheme } from './theme/useTheme';
-
+  ActionButton,
+  type ActionButtonProps,
+} from './components/ActionButton';
 export { Badge, type BadgeProps } from './components/Badge';
 export { Box, type BoxProps } from './components/Box';
 export { Button, type ButtonProps } from './components/Button';
 export { Card, type CardProps } from './components/Card';
+export { Checkbox, type CheckboxProps } from './components/Checkbox';
+export { CloseButton, type CloseButtonProps } from './components/CloseButton';
+export { Container, type ContainerProps } from './components/Container';
 export { Divider, type DividerProps } from './components/Divider';
 export { Flex, type FlexProps } from './components/Flex';
 export { Grid, type GridProps } from './components/Grid';
 export { Heading, type HeadingProps } from './components/Heading';
-export { Image, type ImageProps } from './components/Image';
-export { Input, type InputProps } from './components/Input';
-export { Label, type LabelProps } from './components/Label';
-export { Link, type LinkProps } from './components/Link';
-export {
-  LinearProgress,
-  type LinearProgressProps,
-} from './components/LinearProgress';
-export { Text, type TextProps } from './components/Text';
-export {
-  Select,
-  type SelectProps,
-  type SelectOptions,
-  type SelectOption,
-} from './components/Select';
-export { Spinner, type SpinnerProps } from './components/Spinner';
-export { Radio, type RadioProps } from './components/Radio';
-export { IconButton, type IconButtonProps } from './components/IconButton';
-export { Slider, type SliderProps } from './components/Slider';
-export { Checkbox, type CheckboxProps } from './components/Checkbox';
-export { InfiniteLinearProgress } from './components/InfiniteLinearProgress';
-export { Textarea, type TextareaProps } from './components/Textarea';
-export { Container, type ContainerProps } from './components/Container';
 export { HelpText, type HelpTextProps } from './components/HelpText';
-export { CloseButton, type CloseButtonProps } from './components/CloseButton';
+export { IconButton, type IconButtonProps } from './components/IconButton';
+export { Image, type ImageProps } from './components/Image';
+export { InfiniteLinearProgress } from './components/InfiniteLinearProgress';
+export { Input, type InputProps } from './components/Input';
 export { InputNumber, type InputNumberProps } from './components/InputNumber';
-export { Stack, type StackProps } from './components/Stack';
-export { Paragraph, type ParagraphProps } from './components/Paragraph';
 export {
   InputPassword,
   type InputPasswordProps,
 } from './components/InputPassword/InputPassword';
+export { Label, type LabelProps } from './components/Label';
 export {
-  ActionButton,
-  type ActionButtonProps,
-} from './components/ActionButton';
+  LinearProgress,
+  type LinearProgressProps,
+} from './components/LinearProgress';
+export { Link, type LinkProps } from './components/Link';
+export { Paragraph, type ParagraphProps } from './components/Paragraph';
+export { Radio, type RadioProps } from './components/Radio';
+export {
+  Select,
+  type SelectOption,
+  type SelectOptions,
+  type SelectProps,
+} from './components/Select';
+export { Slider, type SliderProps } from './components/Slider';
+export { Spinner, type SpinnerProps } from './components/Spinner';
+export { Stack, type StackProps } from './components/Stack';
+export { Text, type TextProps } from './components/Text';
+export { Textarea, type TextareaProps } from './components/Textarea';
+export { ThemeProvider, type ThemeProviderProps } from './theme/ThemeProvider';
+export { useTheme } from './theme/useTheme';
+export { type Keyframes, keyframes } from '@emotion/react';
+export { useBreakpointIndex, useResponsiveValue } from '@theme-ui/match-media';
+export {
+  BaseStyles,
+  Global,
+  type SxProp,
+  type Theme,
+  type ThemeUIStyleObject,
+} from 'theme-ui';
