ca2d91d079e15d234d8e34e585147dab3c9afa74
GiovaneGuimaraes <gioguima24@gmail.com>
2025-01-08 16:43:19 -0300

    fix: change animation const name

--- packages/layouts/src/components/Sidebar.tsx
+++ packages/layouts/src/components/Sidebar.tsx
@@ -2,19 +2,19 @@ import { Box, BoxProps, keyframes } from '@ttoss/ui';
 
 import { useGlobal } from './GlobalProvider';
 
-const fadeIn = keyframes({
+const openSidebar = keyframes({
   from: { width: 0 },
   to: { width: '2xs' },
 });
 
 export const Sidebar = (props: BoxProps) => {
   const { isSidebarOpen } = useGlobal();
+
   return (
     <Box
       sx={{
         width: isSidebarOpen ? '2xs' : 0,
-        animation: isSidebarOpen && `${fadeIn} 0.5s ease`,
-
+        animation: isSidebarOpen && `${openSidebar} 1s ease`,
         ...props.sx,
       }}
     >
