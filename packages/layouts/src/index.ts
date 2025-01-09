dbd9c9c20da7f2281567479fd96a2e7c7503fd91
Pedro Arantes <arantespp@gmail.com>
2025-01-08 21:13:42 -0300

    fix: tests

--- packages/layouts/src/index.ts
+++ packages/layouts/src/index.ts
@@ -1,7 +1,3 @@
-export { Footer } from './components/Footer';
-export { Header } from './components/Header';
 export { Layout } from './components/Layout';
-export { GlobalProvider, useGlobal } from './components/LayoutProvider';
-export { Main } from './components/Main';
 export { SidebarCollapseLayout } from './components/SidebarCollapseLayout';
 export { StackedLayout } from './components/StackedLayout';
--- packages/layouts/tests/unit/tests/StackedLayout.test.tsx
+++ packages/layouts/tests/unit/tests/StackedLayout.test.tsx
@@ -1,28 +1,12 @@
 import { render, screen } from '@ttoss/test-utils';
-import { Footer, Header, Main, StackedLayout } from 'src/index';
+import { Layout, StackedLayout } from 'src/index';
 
 test('should render stacked layout from StackedLayout components', () => {
   render(
     <StackedLayout>
-      <Header>Header</Header>
-      <Main>Main</Main>
-      <Footer>Footer</Footer>
-      <div>Extra</div>
-    </StackedLayout>
-  );
-
-  expect(screen.getByText('Header')).toBeInTheDocument();
-  expect(screen.getByText('Main')).toBeInTheDocument();
-  expect(screen.getByText('Footer')).toBeInTheDocument();
-  expect(screen.queryByText('Extra')).not.toBeInTheDocument();
-});
-
-test('should render stacked layout from Layout component', () => {
-  render(
-    <StackedLayout>
-      <Header>Header</Header>
-      <Main>Main</Main>
-      <Footer>Footer</Footer>
+      <Layout.Header>Header</Layout.Header>
+      <Layout.Main>Main</Layout.Main>
+      <Layout.Footer>Footer</Layout.Footer>
       <div>Extra</div>
     </StackedLayout>
   );
