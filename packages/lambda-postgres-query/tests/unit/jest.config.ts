63c00fcea10a61bc581ccddaebdddc7b47cfccfc
Pedro Arantes <arantespp@gmail.com>
2025-01-20 19:30:18 -0300

    fix: lambda query text decoder and error message (#653)
    * fix: lambda query text decoder and error message
    
    * chore: update packages

--- packages/lambda-postgres-query/tests/unit/jest.config.ts
+++ packages/lambda-postgres-query/tests/unit/jest.config.ts
@@ -1,14 +1,3 @@
 import { jestUnitConfig } from '@ttoss/config';
 
-// /**
-//  * https://github.com/facebook/jest/issues/12984#issuecomment-1228392944
-//  */
-// const esmModules = [];
-
-// const transformIgnorePatterns = [
-//   `node_modules/(?!(?:.pnpm/)?(${esmModules.join('|')}))`,
-// ];
-
-export default jestUnitConfig({
-  // transformIgnorePatterns,
-});
+export default jestUnitConfig({});
