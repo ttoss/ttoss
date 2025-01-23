5f76e3d8a09ea5145f6254b0be71b209492b0628
Pedro Arantes <arantespp@gmail.com>
2025-01-23 06:02:47 -0300

    test: add ui label tests

--- packages/ui/tests/unit/tests/Label.test.tsx
+++ packages/ui/tests/unit/tests/Label.test.tsx
@@ -4,6 +4,8 @@ import { Label } from '../../../src';
 
 const LABEL_CONTENT = 'Label text';
 
+const tooltip = 'Tooltip text';
+
 test('should render Label', () => {
   render(<Label>{LABEL_CONTENT}</Label>);
 
@@ -33,4 +35,26 @@ test('should call function onTooltipClick when click on it', async () => {
   await user.click(icon);
 
   expect(onTooltipClick).toHaveBeenCalled();
+});
+
+test('should render tooltip when hover on it', async () => {
+  const user = userEvent.setup({ delay: null });
+  render(<Label tooltip={tooltip}>{LABEL_CONTENT}</Label>);
+
+  const label = screen.getByText(LABEL_CONTENT);
+
+  await user.hover(label);
+
+  expect(screen.getByText(tooltip)).toBeInTheDocument();
+});
+
+test('should not render tooltip when hover on it and tooltip is not provided', async () => {
+  const user = userEvent.setup({ delay: null });
+  render(<Label>{LABEL_CONTENT}</Label>);
+
+  const label = screen.getByText(LABEL_CONTENT);
+
+  await user.hover(label);
+
+  expect(screen.queryByText(tooltip)).not.toBeInTheDocument();
 });
