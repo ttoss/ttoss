bedbb8f2e9eb6fa71f37989aa3a5e04d2b7aa3b8
GiovaneGuimaraes <gioguima24@gmail.com>
2025-01-31 15:20:49 -0300

    fix: changing oca spacing tokens and label text color

--- packages/theme/src/themes/Oca/OcaSpacing.ts
+++ packages/theme/src/themes/Oca/OcaSpacing.ts
@@ -3,8 +3,8 @@ export const ocaSpace = Object.freeze({
   1: '0.25rem', // 4px
   2: '0.375rem', // 6px
   3: '0.5rem', // 8px
-  4: '0.5rem', // 8px
-  5: '0.625rem', // 10px
+  4: '0.5625rem', // 9px
+  5: '0.75rem', // 12px
   6: '1rem', // 16px
   7: '1.25rem', // 20px
   8: '1.5rem', // 24px
