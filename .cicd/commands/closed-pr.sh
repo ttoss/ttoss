eacefb87573ad1ff55cd85c3366a5278b307d9d9
Pedro Arantes <arantespp@gmail.com>
2025-06-20 16:15:43 -0300

    fix: closed pr commands

--- .cicd/commands/closed-pr.sh
+++ .cicd/commands/closed-pr.sh
@@ -1,7 +1,7 @@
 export CARLIN_DESTROY=true
 
-# Need to run build config and carlin before deploy because we're using the flag --only.
-pnpm turbo run build --filter=@ttoss/config --only
+# Need to run build-config and carlin before deploy because we're using the flag --only.
+pnpm turbo run build-config
 pnpm turbo run build --filter=carlin --only
 
 pnpm turbo run deploy --only
