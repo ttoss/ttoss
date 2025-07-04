90efbc6aaae2f0230a78499a5984d48ca54b5502
Pedro Arantes <arantespp@gmail.com>
2025-07-04 15:01:39 -0300

    chore: update packages (#710)

--- packages/postgresdb-cli/src/erd.ts
+++ packages/postgresdb-cli/src/erd.ts
@@ -4,11 +4,17 @@ import sequelizeErd from 'sequelize-erd';
 
 import { getDbDynamically } from './getDbDynamically';
 
-export const erd = async ({ dbPath }: { dbPath: string }) => {
+export const erd = async ({
+  dbPath,
+  engine,
+}: {
+  dbPath: string;
+  engine: string;
+}) => {
   // eslint-disable-next-line no-console
   console.info('Generating ERD...', dbPath);
   const db = await getDbDynamically({ dbPath });
-  const svg = await sequelizeErd({ source: db.sequelize });
+  const svg = await sequelizeErd({ source: db.sequelize, engine });
   await fs.promises.writeFile('erd.svg', svg);
   // eslint-disable-next-line no-console
   console.info('ERD generated at erd.svg');
