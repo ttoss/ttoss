be88966164fc77dde72c9fa9deb19ea22bb85d1e
VRoston <victor.roston@outlook.com>
2025-10-15 12:18:47 -0300

    fix: remove box from menu storybook

--- docs/storybook/stories/components/Menu.stories.tsx
+++ docs/storybook/stories/components/Menu.stories.tsx
@@ -2,7 +2,7 @@ import { Meta, StoryFn } from '@storybook/react-webpack5';
 import { Menu } from '@ttoss/components/Menu';
 import { NotificationCard } from '@ttoss/components/NotificationCard';
 import { Icon } from '@ttoss/react-icons';
-import { Box, Flex } from '@ttoss/ui';
+import { Flex } from '@ttoss/ui';
 
 export default {
   title: 'Components/Menu',
@@ -34,88 +34,72 @@ const menuItems: MenuItem[] = [
 
 export const Example: StoryFn = () => {
   return (
-    <Box
-    //sx={{
-    //  position: 'fixed',
-    //  top: '10px',
-    //  right: '10px',
-    //}}
-    >
-      <Menu>
-        {menuItems.map((item) => {
-          return (
+    <Menu>
+      {menuItems.map((item) => {
+        return (
+          <Flex
+            key={item.label}
+            onClick={() => {
+              return window.open(item.path, '_blank');
+            }}
+            sx={{
+              backgroundColor: 'input.background.muted.default',
+              textDecoration: 'none',
+              borderRadius: 'md',
+              padding: '3',
+              color: 'display.text.secondary.default',
+              alignItems: 'center',
+              cursor: 'pointer',
+              transition: 'all 0.2s ease',
+              '&:hover': {
+                backgroundColor: 'display.background.secondary.default',
+                transform: 'translateX(4px)',
+              },
+            }}
+          >
             <Flex
-              key={item.label}
-              onClick={() => {
-                return window.open(item.path, '_blank');
-              }}
               sx={{
-                backgroundColor: 'input.background.muted.default',
-                textDecoration: 'none',
-                borderRadius: 'md',
-                padding: '3',
-                color: 'display.text.secondary.default',
                 alignItems: 'center',
+                marginLeft: '4',
                 cursor: 'pointer',
-                transition: 'all 0.2s ease',
-                '&:hover': {
-                  backgroundColor: 'display.background.secondary.default',
-                  transform: 'translateX(4px)',
-                },
+                fontSize: 'md',
+                gap: '2',
               }}
             >
-              <Flex
-                sx={{
-                  alignItems: 'center',
-                  marginLeft: '4',
-                  cursor: 'pointer',
-                  fontSize: 'md',
-                  gap: '2',
-                }}
-              >
-                <Icon icon={item.icon} width={18} height={18} />
-                {item.label}
-              </Flex>
+              <Icon icon={item.icon} width={18} height={18} />
+              {item.label}
             </Flex>
-          );
-        })}
-      </Menu>
-    </Box>
+          </Flex>
+        );
+      })}
+    </Menu>
   );
 };
 
 export const WithNotificationCard: StoryFn = () => {
   return (
-    <Box
+    <Menu
+      menuIcon="mdi:bell-outline"
       sx={{
-        position: 'fixed',
-        top: '10px',
-        right: '500px',
+        width: '600px',
       }}
     >
-      <Menu
-        menuIcon="mdi:bell-outline"
-        sx={{
-          width: '600px',
+      <NotificationCard
+        title="Notificação de Exemplo"
+        message="Esta é uma notificação de exemplo para demonstrar o componente NotificationCard dentro do Menu."
+        onClose={() => {
+          return alert('Fechar notificação');
         }}
-      >
-        <NotificationCard
-          title="Notificação de Exemplo"
-          message="Esta é uma notificação de exemplo para demonstrar o componente NotificationCard dentro do Menu."
-          onClose={() => {
-            return alert('Fechar notificação');
-          }}
-          actions={[
-            {
-              action: 'open_url',
-              label: 'Acessar',
-              url: 'https://example.com',
-            },
-          ]}
-          caption="Enviada há 2 horas"
-          type={'success'}
-        />
-      </Menu>
-    </Box>
+        actions={[
+          {
+            action: 'open_url',
+            label: 'Acessar',
+            url: 'https://example.com',
+          },
+        ]}
+        caption="Enviada há 2 horas"
+        type={'success'}
+      />
+    </Menu>
   );
 };
