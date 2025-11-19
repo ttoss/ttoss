3e61935e2a771fcaf04709adf40880b5fad04691
Pedro Arantes <arantespp@gmail.com>
2025-11-19 10:05:31 -0300

    fix: tests

--- packages/react-feature-flags/tests/unit/jest.config.ts
+++ packages/react-feature-flags/tests/unit/jest.config.ts
@@ -1,7 +1,11 @@
 import { jestUnitConfig } from '@ttoss/config';
+import { getTransformIgnorePatterns } from '@ttoss/test-utils';
 
 const config = jestUnitConfig({
   testEnvironment: 'jsdom',
+  transformIgnorePatterns: getTransformIgnorePatterns({
+    esmModules: ['react-error-boundary'],
+  }),
 });
 
 export default config;
