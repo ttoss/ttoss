253ff61c102bca1c32f1737cc41e67ea36c42e37
Pedro Arantes <arantespp@gmail.com>
2025-11-22 08:56:50 -0300

    chore: change config file folder

--- packages/read-config-file/tests/fixtures/importingPackage.ts
+++ packages/read-config-file/tests/fixtures/importingPackage.ts
@@ -1,6 +1,5 @@
-import { operations } from '@ttoss/read-config-file-test';
+import { operations } from '@fixtures/config-file-ts';
 
 const sum = operations.sum + operations.sub;
 
-// eslint-disable-next-line import/no-default-export
 export default { sum };
