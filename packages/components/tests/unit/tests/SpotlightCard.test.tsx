3ab5d97c936d5fbdf5a8678757f0ccbb546496f1
VRoston <victor.roston@outlook.com>
2025-11-27 12:03:31 -0300

    feat: add SpotlightCard to package exports and update import paths in tests

--- packages/components/tests/unit/tests/SpotlightCard.test.tsx
+++ packages/components/tests/unit/tests/SpotlightCard.test.tsx
@@ -1,6 +1,5 @@
+import { SpotlightCard } from '@ttoss/components/SpotlightCard';
 import { fireEvent, render, screen } from '@ttoss/test-utils/react';
-
-import { SpotlightCard } from '../../../src/components/SpotlightCard/SpotlightCard';
 
 describe('SpotlightCard', () => {
   test('should render SpotlightCard with essential props (no buttons)', () => {
