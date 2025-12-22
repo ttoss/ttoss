3af6dded7b9f0fe7027bd9740954fd9d83aaf332
Victor de Melo Roston <111363981+VRoston@users.noreply.github.com>
2025-12-22 14:52:04 -0300

    feat: segmented control variant and form field (#811)
    * feat: segmented control variant and form field
    
    * feat: new test for variant segmented control
    
    * feat: test for form field
    
    * fix: documentation and test
    
    * fix: segmented control

---
+++ packages/forms/src/FormFieldSegmentedControl.tsx
@@ -0,0 +1,68 @@
+import { SegmentedControl, type SegmentedControlProps } from '@ttoss/ui';
+import type { FieldPath, FieldPathValue, FieldValues } from 'react-hook-form';
+
+import { FormField, type FormFieldProps } from './FormField';
+
+export type FormFieldSegmentedControlProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+> = FormFieldProps<TFieldValues, TName> &
+  Omit<SegmentedControlProps, 'value' | 'className'>;
+
+export const FormFieldSegmentedControl = <
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>({
+  defaultValue = '' as FieldPathValue<TFieldValues, TName>,
+  disabled,
+  ...props
+}: FormFieldSegmentedControlProps<TFieldValues, TName>) => {
+  const {
+    label,
+    name,
+    labelTooltip,
+    warning,
+    sx: fieldSx,
+    css,
+    rules,
+    id,
+    auxiliaryCheckbox,
+    onChange,
+    variant = 'secondary',
+    ...segmentedProps
+  } = props;
+
+  return (
+    <FormField
+      id={id}
+      label={label}
+      name={name}
+      labelTooltip={labelTooltip}
+      warning={warning}
+      sx={fieldSx}
+      css={css}
+      defaultValue={defaultValue}
+      rules={rules}
+      disabled={disabled}
+      auxiliaryCheckbox={auxiliaryCheckbox}
+      render={({ field, fieldState }) => {
+        return (
+          <SegmentedControl
+            {...segmentedProps}
+            {...field}
+            variant={variant}
+            value={field.value}
+            onChange={(v: string | number) => {
+              field.onChange(v);
+              onChange?.(v);
+            }}
+            disabled={disabled ?? field.disabled}
+            aria-invalid={fieldState.error ? 'true' : undefined}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default FormFieldSegmentedControl;
