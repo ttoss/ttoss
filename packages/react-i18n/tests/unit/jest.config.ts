857618f09d4f4c71fab496fab25a32067793911d
Pedro Arantes <arantespp@gmail.com>
2026-04-01 10:28:55 -0300

    chore: update formatjs packages

--- packages/react-i18n/tests/unit/jest.config.ts
+++ packages/react-i18n/tests/unit/jest.config.ts
@@ -1,8 +1,12 @@
 import { jestUnitConfig } from '@ttoss/config';
+import { getTransformIgnorePatterns } from '@ttoss/test-utils';
 
 const config = jestUnitConfig({
   setupFilesAfterEnv: ['./setupTests.tsx'],
   testEnvironment: 'jsdom',
+  transformIgnorePatterns: getTransformIgnorePatterns({
+    esmModules: ['react-intl', '@formatjs', 'intl-messageformat'],
+  }),
 });
 
 export default config;
