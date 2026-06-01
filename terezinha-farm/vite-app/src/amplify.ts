554bbddda7759cf2e04cdfe3a385a8d177337250
GiovaneGuimaraes <gioguima24@gmail.com>
2026-06-01 11:16:26 -0300

    fix: improve notifications app page

--- terezinha-farm/vite-app/src/amplify.ts
+++ terezinha-farm/vite-app/src/amplify.ts
@@ -2,7 +2,18 @@ import { Amplify, type ResourcesConfig } from 'aws-amplify';
 import { cognitoUserPoolsTokenProvider } from 'aws-amplify/auth/cognito';
 import { sessionStorage } from 'aws-amplify/utils';
 
+const graphQLRegion =
+  import.meta.env.VITE_APPSYNC_REGION ||
+  import.meta.env.VITE_USER_POOL_ID.split('_')[0];
+
 const config: ResourcesConfig = {
+  API: {
+    GraphQL: {
+      defaultAuthMode: 'userPool',
+      endpoint: import.meta.env.VITE_APPSYNC_GRAPHQL_ENDPOINT,
+      region: graphQLRegion,
+    },
+  },
   Auth: {
     Cognito: {
       userPoolId: import.meta.env.VITE_USER_POOL_ID,
